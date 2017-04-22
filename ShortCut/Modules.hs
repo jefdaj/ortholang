216be4c07d9bd7f091985e93d327d9d120aa5546
Jeffrey David Johnson <jefdaj@berkeley.edu>
2017-04-22 10:49:38 -0700

    rename Sets -> SetOps

--- ShortCut/Modules.hs
+++ ShortCut/Modules.hs
@@ -2,9 +2,9 @@ module ShortCut.Modules where
 
 import ShortCut.Core.Types (CutModule)
 
-import qualified ShortCut.Modules.Math  as M
-import qualified ShortCut.Modules.Sets  as S
-import qualified ShortCut.Modules.Blast as B
+import qualified ShortCut.Modules.Math   as M
+import qualified ShortCut.Modules.SetOps as S
+import qualified ShortCut.Modules.Blast  as B
 
 modules :: [CutModule]
 modules =
