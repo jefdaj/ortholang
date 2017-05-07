28b26180387e81e96d09bea28088dbb34b1ea8d6
Jeffrey David Johnson <jefdaj@berkeley.edu>
2017-05-07 02:03:36 -0700

    wrote cParseSearches

--- ShortCut/Modules/BioMartR.hs
+++ ShortCut/Modules/BioMartR.hs
@@ -11,68 +11,44 @@ import ShortCut.Core.Types
 import ShortCut.Modules.Blast (gom) -- TODO fix that/deprecate
 import Development.Shake
 import ShortCut.Core.Parse (defaultTypeCheck)
-import Control.Monad (void)
+import ShortCut.Core.Compile (cExpr, hashedTmp)
+import Control.Monad (void, when)
 import Text.Parsec            (spaces, runParser)
 import Text.Parsec (Parsec, try, choice, (<|>), many1)
 import Text.Parsec.Char (char, string, alphaNum, oneOf)
 import Text.Parsec.Combinator (between, optionMaybe)
+import Data.Maybe (fromMaybe)
+import Data.List (intercalate)
+import Data.Either (partitionEithers)
+import Data.Char (isSpace)
 
-type Parser a = Parsec String () a
-
-newtype Species = Species String
-newtype DbName  = DbName  String
-newtype DbIden  = DbIden  String
-data Search  = Search Species (Maybe DbName) (Maybe DbIden)
-
-pSpecies :: Parser Species
-pSpecies = fmap Species $ many1 $ alphaNum <|> char '.'
-
--- TODO be stricter in only accepting valid IDs here?
-pDbIden :: Parser DbIden
-pDbIden = fmap DbIden $ many1 $ alphaNum <|> oneOf "_."
-
--- TODO convert to lowercase first?
-pDbName :: Parser DbName
-pDbName = fmap DbName $ choice $ map (try . string)
-  [ "genbank"
-  , "refseq"
-  , "ensembl"
-  , "ensemblgenomes"
-  ]
-
-pFilters :: Parser (Maybe DbName, Maybe DbIden)
-pFilters = do
-  -- TODO fail on empty parens?
-  name <- optionMaybe $ try $ pDbName <* spaces -- TODO leading space OK?
-  iden <- optionMaybe pDbIden -- TODO trailing space OK?
-  return (name, iden)
-
-pSearch :: Parser Search
-pSearch = do
-  species <- pSpecies
-  filters <- optionMaybe $ inParens pFilters
-  case filters of
-    Nothing    -> return $ Search species Nothing Nothing
-    Just (n,i) -> return $ Search species n i
-  where
-    inParens = between (pSym '(') (pSym ')')
-    pSym     = void . char 
+------------------------
+-- module description --
+------------------------
 
 cutModule :: CutModule
 cutModule = CutModule
   { mName = "biomartr"
   , mFunctions =
-    [ getGenome
-    , getGenomes
+    [ parseSearch -- TODO hide from end users?
+    -- , getGenome
+    -- , getGenomes
     ]
   }
 
--- TODO is this needed, since we're just doing stuff in the compiler?
 search :: CutType
 search = CutType
-  { tExt  = "tsv" -- TODO something custom like .search or .bms?
-  , tDesc = "table describing biomartr searches"
+  { tExt  = "tsv" -- TODO should these be unique?
+  , tDesc = "intermediate table describing biomartr searches"
   , tCat  = id
+  }
+
+parseSearch :: CutFunction
+parseSearch = CutFunction
+  { fName      = "parse_search"
+  , fTypeCheck = defaultTypeCheck [str] search
+  , fFixity    = Prefix
+  , fCompiler  = cParseSearches
   }
 
 getGenome :: CutFunction
@@ -91,15 +67,97 @@ getGenomes = CutFunction
   , fCompiler  = cGetGenomes
   }
 
--- runParser :: Parser a -> String -> Either ParseError a
--- runParser p = runParser p "somefile"
+--------------------------
+-- parse search strings --
+--------------------------
+
+type Parser a = Parsec String () a
+
+type Species    = String
+type Database   = String
+type Identifier = String
+
+data Search = Search Species (Maybe Database) (Maybe Identifier)
+
+-- from http://stackoverflow.com/a/6270337
+trim :: String -> String
+trim = f . f
+   where f = reverse . dropWhile isSpace
+
+pSpecies :: Parser Species
+pSpecies = fmap trim $ many1 $ alphaNum <|> oneOf ". "
+
+-- TODO be stricter in only accepting valid IDs here?
+-- TODO decide database from ID?
+pIden :: Parser Identifier
+pIden = many1 $ alphaNum <|> oneOf "._"
+
+-- TODO convert to lowercase first?
+pName :: Parser Database
+pName = choice $ map (try . string)
+  [ "genbank"
+  , "refseq"
+  , "ensembl"
+  , "ensemblgenomes"
+  ]
+
+pFilters :: Parser (Maybe Database, Maybe Identifier)
+pFilters = do
+  -- TODO fail on empty parens?
+  name <- optionMaybe $ try $ pName <* spaces -- TODO leading space OK?
+  iden <- optionMaybe pIden -- TODO trailing space OK?
+  return (name, iden)
+
+pSearch :: Parser Search
+pSearch = do
+  species <- pSpecies
+  filters <- optionMaybe $ inParens pFilters
+  case filters of
+    Nothing    -> return $ Search species Nothing Nothing
+    Just (n,i) -> return $ Search species n i
+  where
+    inParens = between (pSym '(') (pSym ')')
+    pSym     = void . char 
+
+readSearch :: FilePath -> IO (Either String Search)
+readSearch p = do
+  txt <- readFile p
+  return $ case runParser pSearch () "search string" txt of
+    Left  e -> Left  $ show e
+    Right s -> Right $ s
+
+-- TODO use cassava?
+toTsv :: [Search] -> String
+toTsv ss = unlines $ map (intercalate "\t") (header:map row ss)
+  where
+    header             = ["species", "db", "id"]
+    row (Search s d i) = [s, fromMaybe "NA" d, fromMaybe "NA" i]
+
+cParseSearches :: CutConfig -> CutExpr -> Rules FilePath
+cParseSearches cfg expr@(CutFun _ _ ss) = do
+  sFiles <- mapM (cExpr cfg) ss
+  let sTable = hashedTmp cfg expr sFiles
+  sTable %> \out -> do
+    need sFiles
+    parses <- liftIO $ mapM readSearch sFiles
+    let (errors, searches) = partitionEithers parses
+        tsv = toTsv searches
+    -- TODO better error here
+    when (not $ null errors) (error "invalid search!")
+    liftIO $ writeFile out tsv
+  return sTable
+cParseSearches _ _ = error "bad arguments to cParseSearches"
+
+-----------------
+-- get genomes --
+-----------------
 
 -- TODO this is where to parse the searches?
 cGetGenome :: CutConfig -> CutExpr -> Rules FilePath
 cGetGenome cfg expr@(CutFun _ _ [s]) = undefined
-cGetGenome cfg _ = error "bad cGetGenome call"
+cGetGenome _ _ = error "bad cGetGenome call"
 
 -- TODO this is where to parse the searches?
 cGetGenomes :: CutConfig -> CutExpr -> Rules FilePath
 cGetGenomes cfg expr@(CutFun _ _ ss) = undefined
-cGetGenomes cfg _ = error "bad cGetGenomes call"
+cGetGenomes _ _ = error "bad cGetGenomes call"
