0659bcaa8480853259ba42618b3531feeb099d0b
Jeffrey David Johnson <jefdaj@berkeley.edu>
2017-05-07 22:47:04 -0700

    start writing biomartr.R

--- scripts/biomartr.R
+++ scripts/biomartr.R
@@ -1,19 +1,27 @@
 #!/usr/bin/env Rscript
 
+# TODO: hardcode args for development
+# TODO: put downloads in tmpDir (will be created by Shake?)
+# TODO: search specific databases and filter by identifier
+#       (build up args as list, then use do.call?)
+# TODO: nice messages when more than one genome/whatever found
+# TODO: will this work with things other than genomes??
+
 suppressPackageStartupMessages(require(biomartr))
 suppressPackageStartupMessages(require(dplyr))
 
-assert_single_matches <- function(fnName, schTable) {
+assert_single_matches <- function(schTable) {
   # Checks that each row in the search table has a single match
-  # (does this work with things besides genomes?)
-  cat(paste(c("assert_single:", fnName, schTable, "\n")))
+  apply(schTable, 1, function(row) stopifnot(
+    nrow(is.genome.available(row["organism"], details=TRUE)) == 1
+  ))
 }
 
-download_matches <- function(tmpDir, fnName, search) {
+download_matches <- function(tmpDir, fnName, schTable)
   # Calls biomartr and collects the output files
-  cat(paste("download_matches:", tmpDir, fnName, search, "\n"))
-  return(list(paste(c(tmpDir, "fakematch.fna.gz"))))
-}
+  apply(schTable, 1, function(row)
+    getGenome(row["organism"], db=row["database"])
+  )
 
 save_list <- function(files, outPath) {
   # TODO can this be made generic? (later)
@@ -23,18 +31,20 @@ save_list <- function(files, outPath) {
 main <- function() {
   args <- commandArgs(trailingOnly = TRUE)
 
-  tmpDir   <- args[[1]] # tmp directory (shared across biomartr calls)
-  outPath  <- args[[2]] # where to write the list of downloaded files
-  fnName   <- args[[3]] # name of the function to call (getGenome etc)
-  schTable <- args[[4]] # search table (species, optional db, optional id)
+  tmpDir   <- args[[1]]
+  outPath  <- args[[2]]
+  fnName   <- readLines(args[[3]])
+  schTable <- read.table(args[[4]], sep="\t", header=TRUE) 
 
-  cat(paste(c("tmpDir:"  , tmpDir  , "\n")))
-  cat(paste(c("outPath:" , outPath , "\n"))) # TODO multiple instead?
-  cat(paste(c("fnName:"  , fnName  , "\n")))
-  cat(paste(c("schTable:", schTable, "\n"))) # TODO fix cache thing
+  print(tmpDir)
+  print(outPath)
+  print(fnName)
+  print(schTable)
 
   # assert_single_matches(fnName, schTable)
-  # download_matches(tmpDir, fnName, search) %>% save_list(outPath)
+  # download_matches(tmpDir, fnName, schTable) %>% save_list(outPath)
+  assert_single_matches(schTable)
+  download_matches(tmpDir, fnName, schTable)
 }
 
 main()
