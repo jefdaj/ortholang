57cb75635cf1fbdfc40029ea90a2820172f7b074
Jeffrey David Johnson <jefdaj@berkeley.edu>
2017-06-24 09:50:01 -0700

    cleanup and notes

--- scripts/shmlast.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# TODO will this work without matplotlib 1.5.1? apparently seaborn has an issue with 1.5.3
-
-from shmlast.last import lastal_task
-
-def task_lastal():
-        return lastal_task('query.fna', 'db.faa', translate=True)
