a2a44416fcefa9651761f559c942b1d13c73cd14
Jeffrey David Johnson <jefdaj@berkeley.edu>
2017-10-06 10:50:48 -0700

    wip refactoring map

--- src/ShortCut/Core/Compile/Map.hs
+++ src/ShortCut/Core/Compile/Map.hs
@@ -52,11 +52,28 @@ rMapTmps fn tmpPrefix s@(_,cfg) e = rMap tmpFn fn tmpPrefix s e
  - tmpfile (without ".args"), and when read they contain a list of paths to
  - pass the action to produce that tmpfile.
  -
- - But is there a simpler way?
+ - But is there a simpler way? (Work on that later)
  -
- - TODO rename to be clearly "each"-related and use .each for the map files
+ - TODO NEXT STEP: put the outtmp in the expr dir of the regular fn!
+ -                 that way it'll automatically dedup at runtime right?
+ -                 wait, how to prevent "does not exist" errors? low priority?
+ -                 to do that, need to move/remove alternatives call
+ -                 ... which doesn't work. fuck.
+ -                 could hack it with extra ".done" files?
+ -                 or hack it with alwaysRerun + doesFileExist?
+ -                 or hack it with a set of "paths already used" in CutState?
+ -                 or just keep to specific files rather than * patterns probably!
  -
- - TODO put the .each in the cachedir of the regular fn
+ -                 also this might help:
+ -                 expr paths should be based only on the expressions!
+ -                 each module can dedup by content using content-hashed files too
+ -                 those go in its cache dir and it just links to them as needed
+ -
+ -                 another way of putting it:
+ -                 expr paths should be a Rules/compile-time thing
+ -                 and cache paths an Actions/runtime thing
+ -                 (distinction useful only if more than one expr per cache file?)
+ -
  - TODO and the final outfile in the expr dir of the regular fn:
  -
  -      cache/<fnname>/<hash of non-mapped args>.each
@@ -74,73 +91,85 @@ rMapTmps fn tmpPrefix s@(_,cfg) e = rMap tmpFn fn tmpPrefix s e
  -     main fn you can calculate it too to make the mapTmp pattern.
  -}
 
+-- TODO centralize argsPath, which argPaths come from
+-- TODO centralize outPaths?
+-- TODO last of argPaths -> final out file?
+-- TODO have aMapElem make a cached outfile, but also link to it from the expr path
+--      that's inelegant but should solve the duplication right?
+
 rMap :: ([CutPath] -> CutPath)
-         -> (CutConfig -> CutPath -> [CutPath] -> Action ())
-         -> String -> RulesFn
+     -> (CutConfig -> CutPath -> [CutPath] -> Action ())
+     -> String -> RulesFn
 rMap tmpFn actFn prefix s@(_,cfg) e@(CutFun _ _ _ _ exprs) = do
-  -- TODO make this an actual debug call
-  -- liftIO $ putStrLn $ "rMap expr: " ++ render (pPrint e)
-  initPaths <- mapM (rExpr s) (init exprs)
-  (ExprPath lastsPath) <- rExpr s (last exprs)
-  let inits    = map (\(ExprPath p) -> toCutPath cfg p) initPaths
-      lasts'   = toCutPath cfg lastsPath
-      outPath  = exprPath s e
-      outPath' = fromCutPath cfg outPath
-      (ListOf t) = typeOf e
-      mapTmp   = (fromCutPath cfg $ cacheDir cfg prefix) </> digest e -- <.> extOf t
-      mapTmp'  = toCutPath cfg mapTmp
-  -- This builds .args files then needs their actual non-.args outpaths, which
-  -- will be built by the action below
-  outPath' %> \_ -> aMapArgs cfg outPath inits mapTmp' t lasts'
-  -- This builds one of the list of out paths based on a .args file
-  -- (made in the action above). It's a pretty roundabout way to do it!
-  -- TODO ask ndmitchell if there's something much more elegant I'm missing
-  (mapTmp </> "*" <.> extOf t) %> aMapTmp cfg tmpFn actFn
-  return $ debugRules cfg "rMap" e $ ExprPath outPath'
+  argInitPaths <- mapM (rExpr s) (init exprs)
+  (ExprPath argsLastsPath) <- rExpr s (last exprs)
+  let mainOutPath    = fromCutPath cfg $ exprPath s e
+      argInitPaths'  = map (\(ExprPath p) -> toCutPath cfg p) argInitPaths
+      argLastsPath'  = toCutPath cfg argsLastsPath
+      elemCacheDir   = (fromCutPath cfg $ cacheDir cfg prefix) </> digest e
+      elemCacheDir'  = toCutPath cfg elemCacheDir -- TODO redundant?
+      elemCachePtn   = elemCacheDir </> "*" <.> extOf eType
+      (ListOf eType) = typeOf e
+  elemCachePtn %> aMapElem cfg tmpFn actFn
+  mainOutPath  %> aMap cfg argInitPaths' elemCacheDir' eType argLastsPath'
+  return $ debugRules cfg "rMap" e $ ExprPath mainOutPath
 rMap _ _ _ _ _ = error "bad argument to rMapTmps"
 
-aMapArgs :: CutConfig -> CutPath -> [CutPath]
-             -> CutPath -> CutType -> CutPath -> Action ()
-aMapArgs cfg outPath inits mapTmp etype lastsPath = do
+-- This builds .args files then needs their actual non-.args outpaths, which
+-- will be built by the action below
+aMap :: CutConfig
+     -> [CutPath] -> CutPath -> CutType -> CutPath -> FilePath
+     -> Action ()
+aMap cfg inits mapTmp eType lastsPath outPath = do
   lastPaths <- readPaths cfg lasts' -- TODO this needs a lit variant?
-  -- this writes the .args files for use in the rule above
-  (flip mapM_) lastPaths $ \p -> do
-    -- TODO write the out path here too so all the args are together?
-    let p'       = fromCutPath cfg p
-        -- TODO use a hash here?
-        argsPath = tmp' </> takeFileName p' -<.> extOf etype <.> "args"
-        argPaths = inits' ++ [p'] -- TODO abs path bug here?
-    liftIO $ createDirectoryIfMissing True $ tmp'
-    -- TODO these aren't lits! are they strings then?
-    writeLits cfg argsPath argPaths
-  -- then we just trigger them and write to the overall outPath
-  let outPaths  = map (\x -> tmp' </> takeBaseName x <.> extOf etype)
+  mapM_ (aMapElemArgs cfg eType inits' tmp') lastPaths
+  let outPaths  = map (\x -> tmp' </> takeBaseName x <.> extOf eType)
                       (map (fromCutPath cfg) lastPaths)
       outPaths' = map (toCutPath cfg) outPaths
   need outPaths
-  let out = debugAction cfg "aMapArgs" out' (out':inits' ++ [tmp', lasts'])
+  let out = debugAction cfg "aMap" outPath (outPath:inits' ++ [tmp', lasts'])
   writePaths cfg out outPaths'
   where
-    out'   = fromCutPath cfg outPath
     inits' = map (fromCutPath cfg) inits
     lasts' = fromCutPath cfg lastsPath
     tmp'   = fromCutPath cfg mapTmp
 
+-- TODO can we remove eType here?
+aMapElemArgs :: CutConfig
+             -> CutType -> [FilePath] -> FilePath -> CutPath
+             -> Action ()
+aMapElemArgs cfg eType inits' tmp' p = do
+  -- TODO write the out path here too so all the args are together?
+  let p'       = fromCutPath cfg p
+      -- TODO use a hash here?
+      argsPath = tmp' </> takeFileName p' -<.> extOf eType <.> "args"
+      argPaths = inits' ++ [p'] -- TODO abs path bug here?
+  liftIO $ createDirectoryIfMissing True $ tmp'
+  -- TODO these aren't lits! are they strings then?
+  writeLits cfg argsPath argPaths
+
+-- This builds one of the list of out paths based on a .args file
+-- (made in the action above). It's a pretty roundabout way to do it!
+-- TODO ask ndmitchell if there's something much more elegant I'm missing
 -- TODO any way to make that last FilePath into a CutPath? does it even matter?
-aMapTmp :: CutConfig
-               -> ([CutPath] -> CutPath)
-               -> (CutConfig -> CutPath -> [CutPath] -> Action a)
-               -> FilePath -> Action ()
-aMapTmp cfg tmpFn actFn out = do
+aMapElem :: CutConfig
+         -> ([CutPath] -> CutPath)
+         -> (CutConfig -> CutPath -> [CutPath] -> Action a)
+         -> FilePath -> Action ()
+aMapElem cfg tmpFn actFn out = do
   let argsPath = out <.> "args" -- TODO clean up
+  -- liftIO $ putStrLn $ "argsPath (aMapElem): " ++ argsPath
   args <- readPaths cfg argsPath
   let args' = map (fromCutPath cfg) args
+  -- liftIO $ putStrLn $ "argPaths (aMapElem): " ++ show args'
   need args'
   -- TODO fix actual tmpFns to use CutPaths (automatically deterministic!)
   let dir = tmpFn args
       dir' = fromCutPath cfg dir
       args'' = (toCutPath cfg out):args
-      out'  = debugAction cfg "aMapTmp" out args' -- TODO is this right?
+      out'  = debugAction cfg "aMapElem" out args' -- TODO is this right?
   liftIO $ createDirectoryIfMissing True dir'
+  -- liftIO $ putStrLn $ "out' (aMapElem): " ++ show out'
+  -- liftIO $ putStrLn $ "args'': " ++ show args''
   _ <- actFn cfg dir args''
   trackWrite [out']
