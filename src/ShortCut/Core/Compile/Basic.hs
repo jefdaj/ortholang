02eab5764b0898b0f416b72d9a439c42db1aacaf
Jeffrey David Johnson <jefdaj@berkeley.edu>
2017-10-11 14:08:36 -0700

    dedup list contents

--- src/ShortCut/Core/Compile/Basic.hs
+++ src/ShortCut/Core/Compile/Basic.hs
@@ -25,9 +25,10 @@ import ShortCut.Core.Paths (cacheDir, exprPath, exprPathExplicit, toCutPath,
                             fromCutPath, varPath, writePaths, CutPath, readLitPaths,
                             readLit, readLits, writeLits)
 
+import Control.Monad               (when)
 import Data.List                   (find, sort, intersperse)
 import Data.Maybe                  (fromJust)
-import Development.Shake.FilePath  ((</>))
+import Development.Shake.FilePath  ((</>), (<.>))
 import ShortCut.Core.Config        (wrappedCmd)
 import ShortCut.Core.Debug         (debugTrackWrite, debugAction, debugRules)
 import ShortCut.Core.Util          (absolutize, resolveSymlinks, stripWhiteSpace,
@@ -144,12 +145,34 @@ rListLits s@(_,cfg) e@(CutList _ _ _ exprs) = do
     outPath' = debugRules cfg "rListLits" e $ fromCutPath cfg outPath
 rListLits _ e = error $ "bad argument to rListLits: " ++ show e
 
+{- This ensures that when two lists have the same content, their expression
+ - paths will be links to the same cached path. That causes them to get
+ - properly deduplicated when used in a set operation. It also makes the .tree
+ - test files much stricter, since they'll change if any list element changes.
+ -
+ - TODO does it need to handle a race condition when writing to the cache?
+ - TODO any reason to keep original extensions instead of all using .txt?
+ -}
+writeDeduped :: Show a => CutConfig
+             -> (CutConfig -> FilePath -> a -> Action ())
+             -> FilePath -> a -> Action ()
+writeDeduped cfg writeFn outPath content = do
+  let cDir     = fromCutPath cfg $ cacheDir cfg "list" -- TODO make relative to expr
+      cache    = cDir </> digest content <.> "txt"
+      cacheRel = ".." </> ".." </> makeRelative (cfgTmpDir cfg) cache
+  done <- doesFileExist cache
+  liftIO $ createDirectoryIfMissing True cDir
+  when (not done) (writeFn cfg cache content)
+  wrappedCmd cfg [outPath] [] "ln" ["-fs", cacheRel, outPath] -- TODO quietly?
+  debugTrackWrite cfg [outPath]
+
+-- TODO put this in a cache dir by content hash and link there
 aListLits :: CutConfig -> [CutPath] -> CutPath -> Action ()
 aListLits cfg paths outPath = do
   need paths'
   lits <- mapM (readLit cfg) paths'
   let lits' = sort $ map stripWhiteSpace lits
-  writeLits cfg out'' lits'
+  writeDeduped cfg writeLits out'' lits'
   where
     out'   = fromCutPath cfg outPath
     out''  = debugAction cfg "aListLits" out' (out':paths')
@@ -174,7 +197,7 @@ aListPaths cfg paths outPath = do
   paths'' <- liftIO $ mapM (resolveSymlinks cfg) paths'
   need paths''
   let paths''' = map (toCutPath cfg) paths''
-  writePaths cfg out'' paths'''
+  writeDeduped cfg writePaths out'' paths'''
   where
     out'   = fromCutPath cfg outPath
     out''  = debugAction cfg "aListPaths" out' (out':paths')
