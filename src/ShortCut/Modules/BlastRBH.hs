62c7741409817623a1b2c269e3fdbe07a7a7452d
Jeffrey David Johnson <jefdaj@berkeley.edu>
2017-10-12 18:10:40 -0700

    draft new *blast*_rbh fns

--- src/ShortCut/Modules/BlastRBH.hs
+++ src/ShortCut/Modules/BlastRBH.hs
@@ -14,16 +14,8 @@ import ShortCut.Modules.Blast      (bht, BlastDesc, blastDescs)
 import ShortCut.Modules.SeqIO      (faa, fna)
 import System.Directory            (createDirectoryIfMissing)
 
--- TODO this module should provide:
---
--- reciprocal_best      :: bht -> bht -> bht
--- reciprocal_best_each :: bht -> [bht] -> [bht]
--- *blast*_rbh          :: fa -> fa -> bht     (for the ones that make sense)
--- *blast*_rbh_each     :: fa -> [fa] -> [bht] (for the ones with an _rbh)
---
 -- TODO should the _rev functions also be moved here?
--- TODO ideally, remove a lot of this crap and the second python script
--- TODO test each one as you go: first all the peices, then together
+-- TODO test each one: first all the peices, then together
 
 cutModule :: CutModule
 cutModule = CutModule
@@ -31,17 +23,10 @@ cutModule = CutModule
   , mFunctions =
     [ reciprocalBest
     , reciprocalBestEach
-    -- old fns to remove:
-    -- , mkBlastSymRBH     "blastn" fna
-    -- , mkBlastSymRBHEach "blastn" fna
-    -- , mkBlastSymRBH     "blastp" faa
-    -- , mkBlastSymRBHEach "blastp" faa
-    ] ++
-
-    -- new fns:
+    ]
     -- TODO filter for the ones that make sense in _rbh form!
-    map mkBlastRbh     blastDescs ++
-    map mkBlastRbhEach blastDescs
+    ++ map mkBlastRbh     blastDescs
+    ++ map mkBlastRbhEach blastDescs
   }
 
 ---------------------
@@ -87,169 +72,38 @@ reciprocalBestEach = CutFunction
 -----------------
 
 mkBlastRbh :: BlastDesc -> CutFunction
-mkBlastRbh (bCmd, qType, sType, _) = CutFunction
+mkBlastRbh d@(bCmd, qType, sType, _) = CutFunction
   { fName      = bCmd ++ "_rbh"
   , fTypeCheck = defaultTypeCheck [num, qType, sType] bht
   , fFixity    = Prefix
-  , fRules     = undefined
+  , fRules     = rMkBlastRbh d
   }
 
--- TODO make the rest of them... but not until after lab meeting?
--- mkBlastSymRBH :: String -> CutType -> CutFunction
--- mkBlastSymRBH name faType = CutFunction
---   { fName = name ++ "_rbh"
---   , fTypeCheck = defaultTypeCheck [num, faType, faType] bht
---   , fFixity = Prefix
---   , fRules = rBlastSymRBH name
---   }
-
--- it this works I'll be modeling new versions of the map ones after it
--- rBlastSymRBH :: String -> RulesFn
--- rBlastSymRBH bCmd s@(_,cfg) e@(CutFun _ salt deps _ [evalue, lfa, rfa]) = do
---   let lhits = CutFun bht salt deps bCmd [lfa , rfa , evalue]
---       rhits = CutFun bht salt deps bCmd [rfa , lfa , evalue]
---       lbest = CutFun bht salt deps "best_hits"  [lhits] -- TODO remove?
---       rbest = CutFun bht salt deps "best_hits"  [rhits] -- TODO remove?
---       rbh   = CutFun bht salt deps "reciprocal_best" [lbest, rbest]
---       out   = fromCutPath cfg $ exprPath s e
---   (ExprPath rbhPath) <- rExpr s rbh -- TODO this is the sticking point right?
---   out %> \_ -> aBlastSymRBH cfg (CacheDir $ fromCutPath cfg $ cacheDir cfg "blast") [ExprPath out, ExprPath rbhPath]
---   return (ExprPath out)
--- rBlastSymRBH _ _ _ = error "bad argument to cBlastSymRBH"
-
--- this is an attempt to convert rBlastpRBH into a form usable with rMapTmp
--- TODO is it just cLink? seems pretty similar!
--- aBlastSymRBH :: ActionFn
--- aBlastSymRBH cfg _ [ExprPath out, ExprPath rbhPath] = do
---   need [rbhPath]
---   let out' = debugAction cfg "aBlastSymRBH" out [out, rbhPath]
---   unit $ quietly $ wrappedCmd cfg [out'] [] "ln" ["-fs", rbhPath, out']
---   debugTrackWrite cfg [out']
--- aBlastSymRBH _ _ args = error $ "bad arguments to aBlastSymRBH: " ++ show args
+-- TODO this only works with symmetric fns so far... either fix or restrict to those!
+rMkBlastRbh :: BlastDesc -> RulesFn
+rMkBlastRbh (bCmd, _, _, _) s (CutFun _ salt deps _ [e, l, r]) = rExpr s main
+  where
+    main  = CutFun bht salt deps "reciprocal_best" [lHits, rHits]
+    lHits = CutFun bht salt deps  bCmd            [e, l, r]
+    rHits = CutFun bht salt deps (bCmd ++ "_rev") [e, l, r]
+rMkBlastRbh _ _ _ = error "bad argument to rMkBlastRbh"
 
 ----------------------
 -- *blast*_rbh_each --
 ----------------------
 
 mkBlastRbhEach :: BlastDesc -> CutFunction
-mkBlastRbhEach (bCmd, qType, sType, _) = CutFunction
+mkBlastRbhEach d@(bCmd, qType, sType, _) = CutFunction
   { fName      = bCmd ++ "_rbh_each"
   , fTypeCheck = defaultTypeCheck [num, qType, ListOf sType] (ListOf bht)
   , fFixity    = Prefix
-  , fRules     = undefined
+  , fRules     = rMkBlastRbhEach d
   }
 
--- TODO remove?
--- reciprocalBestEach :: CutFunction
--- reciprocalBestEach = CutFunction
---   { fName      = "reciprocal_best_each"
---   , fTypeCheck = defaultTypeCheck [bht, ListOf bht] (ListOf bht)
---   , fFixity    = Prefix
---   , fRules  = rRecipEach
---   }
-
--- TODO how to hook this up to blastp_each?
--- rRecipEach :: RulesFn
--- rRecipEach s@(_,cfg) e@(CutFun _ _ _ _ [lbhts, rbhts]) = do
---   (ExprPath lsPath) <- rExpr s lbhts
---   (ExprPath rsPath) <- rExpr s rbhts
---   let lsPath' = toCutPath cfg lsPath
---       rsPath' = toCutPath cfg rsPath
---   oPath' %> \_ -> aReciprocalBestEach cfg oPath lsPath' rsPath' cDir
---   return (ExprPath oPath')
---   where
---     cDir   = cacheDir cfg "reciprocal_best_each"
---     oPath  = exprPath s e
---     oPath' = fromCutPath cfg oPath
--- rRecipEach _ _ = error "bad argument to rRecipEach"
-
--- TODO ARE THE CUTPATHS GETTING THROUGH HERE? YEAH SUBPROCESS.WHATEVER...
--- NEED TO MAKE SOMETHING TO REPLACE ALL THROUGH BEFORE PASSING TO OUTSIDE
--- CODE, KIND OF LIKE FROMSHORTCUTLIST BUT HOPEFULLY A LOT SIMPLER?
---
--- THAT'S TOTALLY DOABLE, BUT WOULD REWRITING THIS SCRIPT IN HASKELL BE
--- EASIER? CERTAINLY SEEMS ARBITRARY TO HAVE JUST THIS ONE PART IN PYTHON.
--- TRY REWRITING IT FIRST I GUESS IN THE MORNING
--- aReciprocalBestEach :: CutConfig -> CutPath -> CutPath -> CutPath -> CutPath -> Action ()
--- aReciprocalBestEach cfg oPath lsPath rsPath cDir = do
---   need [lsPath', rsPath']
---   unit $ quietly $ wrappedCmd cfg [oPath'] [Cwd cDir'] "reciprocal_best_each.py"
---     [cDir', oPath', lsPath', rsPath']
---   debugTrackWrite cfg [oPath'']
---   where
---     oPath'  = fromCutPath cfg oPath
---     lsPath' = fromCutPath cfg lsPath
---     rsPath' = fromCutPath cfg rsPath
---     cDir'   = fromCutPath cfg cDir
---     oPath'' = debugAction cfg "aReciprocalBestEach" oPath' [oPath', rsPath', rsPath', cDir']
-
------------------------------------------------
--- the hard part: mapped reciprocal versions --
------------------------------------------------
-
-
--- TODO find a way to fix this or replace it...
-
--- mkBlastSymRBHEach :: CutFunction
--- mkBlastSymRBHEach = CutFunction
---   { fName      = "blastp_rbh_each"
---   , fTypeCheck = defaultTypeCheck [num, faa, ListOf faa] (ListOf bht)
---   , fFixity    = Prefix
---   , fRules  = rBlastSymRBHEach
---   }
--- 
--- -- TODO oh right, that might not be directly a list!
--- --      can this be done a cleaner way??
--- rBlastSymRBHEach :: RulesFn
--- rBlastSymRBHEach st@(_,cfg) expr@(CutFun _ salt _ _ [e, q, CutList _ _ _ ss]) = do
--- -- rBlastSymRBHEach st@(scr,cfg) expr@(CutFun _ salt _ _ [e, q, ss]) = do
---   -- let subjects = extractExprs scr ss
---   let exprs = map (\s -> CutFun bht salt (concatMap depsOf [e, q, s]) "blastp_rbh" [e, q, s]) ss
---   paths <- mapM (rExpr st) exprs
---   let (ExprPath out) = exprPath cfg True expr []
---       paths' = map (\(ExprPath p) -> p) paths
---   out %> \_ -> need paths' >> debugWriteLines cfg out paths' >> debugTrackWrite cfg [out]
---   return (ExprPath out)
--- rBlastSymRBHEach _ _ = error "bad argument to rBlastSymRBHEach"
-
-
--- mkBlastSymRBHEach :: String -> CutType -> CutFunction
--- mkBlastSymRBHEach name faType = CutFunction
---   { fName      = name ++ "_rbh_each"
---   , fTypeCheck = defaultTypeCheck [num, faType, ListOf faType] (ListOf bht)
---   , fFixity    = Prefix
---   , fRules  = rBlastSymRBHEach name
---   }
-
-
--- TODO how to remove all these files? will their mkBlast... take care of it?
--- rBlastSymRBHEach :: String -> RulesFn
--- rBlastSymRBHEach bCmd s@(_,cfg) e@(CutFun rtn salt deps _ [evalue, query, subjects]) = do
---   -- TODO need to get best_hits on each of the subjects before calling it, or duplicate the code inside?
---   -- let mkExpr name = CutFun bht salt deps "best_hits" [CutFun bht salt deps name [evalue, query, subjects]]
---   let mkExpr name = CutFun rtn salt deps name [evalue, query, subjects]
---       oPath  = exprPath s e
---       oPath' = fromCutPath cfg oPath
---       cDir   = cacheDir cfg "reciprocal_each"
---   (ExprPath fwdsPath) <- rExpr s $ mkExpr $ bCmd ++ "_each"
---   (ExprPath revsPath) <- rExpr s $ mkExpr $ bCmd ++ "_each_rev"
---   let fwdsPath' = toCutPath cfg fwdsPath
---       revsPath' = toCutPath cfg revsPath
---   oPath' %> \_ -> aBlastSymRBHEach cfg oPath cDir fwdsPath' revsPath'
---   return (ExprPath oPath')
--- rBlastSymRBHEach _ _ _ = error "bad argument to rBlastSymRBHEach"
-
--- aBlastSymRBHEach :: CutConfig -> CutPath -> CutPath
---                  -> CutPath -> CutPath -> Action ()
--- aBlastSymRBHEach cfg oPath cDir fwdsPath revsPath = do
---   need [fwdsPath', revsPath']
---   liftIO $ createDirectoryIfMissing True cDir'
---   unit $ quietly $ wrappedCmd cfg [oPath'] [Cwd cDir']
---                        "reciprocal_each.py" [cDir', oPath', fwdsPath', revsPath']
---   debugTrackWrite cfg [oPath'']
---   where
---     oPath'    = fromCutPath cfg oPath
---     cDir'     = fromCutPath cfg cDir
---     fwdsPath' = fromCutPath cfg fwdsPath
---     revsPath' = fromCutPath cfg revsPath
---     oPath'' = debugAction cfg "aBlastSymRBHEach" oPath' [oPath', cDir', fwdsPath', revsPath']
+rMkBlastRbhEach :: BlastDesc -> RulesFn
+rMkBlastRbhEach (bCmd, _, _, _) s (CutFun _ salt deps _ [e, l, rs]) = rExpr s main
+  where
+    main  = CutFun (ListOf bht) salt deps "reciprocal_best_each" [lHits, rHits]
+    lHits = CutFun (ListOf bht) salt deps (bCmd ++ "_each"    )  [e, l, rs]
+    rHits = CutFun (ListOf bht) salt deps (bCmd ++ "_each_rev")  [e, l, rs]
+rMkBlastRbhEach _ _ _ = error "bad argument to rMkBlastRbh"
