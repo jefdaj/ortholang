106f1dfaff44e4ae9c31bb99db2cf769f197d808
Jeffrey David Johnson <jefdaj@berkeley.edu>
2017-11-14 10:01:10 -0800

    minor changes to build script

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # other possible settings: https://hackage.haskell.org/package/tasty
 export TASTY_QUICKCHECK_TESTS=1000
 export TASTY_HIDE_SUCCESSES=True
+[[ -z "$1" ]] || export TASTY_PATTERN="$1"
 
 # Only run certain tests; see https://github.com/feuerbach/tasty#patterns
 # export TASTY_PATTERN=testpatternhere
@@ -12,4 +13,6 @@ export TASTY_HIDE_SUCCESSES=True
 # nix-build && ./result/bin/shortcut --test
 
 # this does an incremental build of the haskell code for faster testing
+export STACK_ROOT=$PWD/src/.stack-work
+mkdir -p $STACK_ROOT
 nix-shell --command '(cd src && stack build && ./.stack-work/install/*/*/*/bin/shortcut --test) || exit'
