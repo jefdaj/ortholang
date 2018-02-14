3d969a3f9ae5378abab6587effc420d083a8b386
Jeffrey David Johnson <jefdaj@berkeley.edu>
2018-02-13 16:09:47 -0800

    wip getting srun wrapper to work

--- wrappers/srun.sh
+++ wrappers/srun.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# TODO It would probably work better to have an overall salloc script run shortcut right?
+#      That way you would know the available resources to set here.
+#      Even if it seems more complicated that's probably worth it for time saved.
+
+# TODO any point controlling number of threads here? or is that crb-blast specific?
+
+# Don't forget to set this before running.
+# TODO test out which values work best (may vary by command)
+CPUS_PER_TASK=4
+TIME_PER_TASK=01:00:00
+
 # This is a wrapper script for running individual commands on other nodes.
 # It's meant to be called from ShortCut with all the same arguments that make
 # up a regular shell command. It runs the command using srun, then waits for a
@@ -20,9 +31,9 @@ log() { [[ -z $SHORTCUT_LOGLEVEL ]] || echo "[$(date '+%Y-%m-%d %H:%M:%S')] $@";
 
 # put any changes needed to optimize specific commands here
 case "$(basename "$1")" in
-	crb-blast) CMD="$SRUN --cpus-per-task=4 --time=99:00:00 $@ --verbose --split --threads=12"; log "$CMD";;
-	parallelblast.py) CMD="$SRUN --cpus-per-task=4 --time=99:00:00 $@"; log "$CMD";;
-	blastn|blastp|blastx|tblastn|tblastx|megablast) CMD="$SRUN --cpus-per-task=4 --time=99:00:00 $@"; log "$CMD";;
+	crb-blast) CMD="$SRUN --cpus-per-task=$CPUS_PER_TASK --time=$TIME_PER_TASK $@ --verbose --split --threads=12"; log "$CMD";;
+	parallelblast.py) CMD="$SRUN --cpus-per-task=$CPUS_PER_TASK --time=$TIME_PER_TASK $@"; log "$CMD";;
+	blastn|blastp|blastx|tblastn|tblastx|megablast) CMD="$SRUN --cpus-per-task=$CPUS_PER_TASK --time=$TIME_PER_TASK $@"; log "$CMD";;
 	cat|ln) CMD="$@";; # TODO does anything echoed here get sent to the output file??
 	*) CMD="$@"; log "$CMD";; # run the command as-is in this shell
 esac
