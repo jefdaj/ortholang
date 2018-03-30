483a00c3ba93c162b324dff559e26f65d18625d7
Jeffrey David Johnson <jefdaj@berkeley.edu>
2018-03-30 13:55:08 -0700

    add proper flock locks to berkeley.sh

--- wrappers/berkeley.sh
+++ wrappers/berkeley.sh
@@ -7,45 +7,71 @@
 # Note that you also need to set TMPDIR to something shared across machines
 # before invoking shortcut. For example I use:
 # export TMPDIR=/global/scratch/jefdaj/shortcut-test
-# Otherwise you'll get lots of srun errors "cannot chdir to /tmp/whatever..."
+# Otherwise you'll get lots of possibly-confusing srun errors!
 
 # TODO set logfile from shortcut config
 
 # Common options for all srun commands
 SRUN="srun --account=co_rosalind --partition=savio2_htc --qos=rosalind_htc2_normal"
 SRUN="$SRUN --chdir $(pwd) --quiet"
+JOBSFILE="/tmp/jobs.txt" # use main tmpdir to share between instances
 
-limit_total_jobs() {
-  # Beyond 100-200 jobs at once, random SLURM bugs seem to surface which is a
-  # bad time for everyone involved! This helps prevent that.
-  #
-  # Note that srun_parallel launches jobs in batches, so you might get as many
-  # as maxjobs + -jwhatever. But if you set -j too low and run one big BLAST
-  # operation, you'll only get -j jobs at once. This script is set to spawn up
-  # to 80 jobs per parallel operation and never more than 180 total.
+start_jobs() {
+  # Try to add jobs to the jobs file, and fail if that exceeds the max.
+  nnewjobs="$1"
+  nmaxjobs=160 # TODO how many are typically going before errors start?
+  (
+    flock -x 13
+    ncurjobs=$(cat "$JOBSFILE")
+    [[ -z $ncurjobs ]] && ncurjobs=0
+    ntotjobs=$(( $ncurjobs + $nnewjobs ))
+    if [[ $ntotjobs -le $nmaxjobs ]]; then
+      echo "$ntotjobs" > "$JOBSFILE"
+      # sync # TODO remove?
+    else
+      return 1
+    fi
+  ) 13>"${JOBSFILE}.lock"
+}
 
-  maxjobs=50
-  while [[ "$(squeue -u $(whoami) | wc -l)" -gt $maxjobs ]]; do
-    sleep 5
+finish_jobs() {
+  # Remove jobs from the jobs file. Should always succeed.
+  ndonejobs="$1"
+  (
+    flock -x 13
+    ncurjobs="$(cat "$JOBSFILE")"
+    ntotjobs="$(( $ncurjobs - $ndonejobs ))"
+    echo "$ntotjobs" > "$JOBSFILE"
+    # sync # TODO remove?
+  ) 13>"${JOBSFILE}.lock"
+}
+
+run() {
+  # Beyond ~150-200 jobs at once, random SLURM bugs seem to surface which is a
+  # bad time for everyone involved! This is super hacky but helps prevent that.
+  njobs="$1"; shift
+  cmd="$@"
+  while true; do
+    start_jobs $njobs && break || sleep 5
   done
+  echo "$cmd" >> /tmp/wrapper.log
+  eval "$cmd; finish_jobs $njobs" 2>> /tmp/wrapper.log
 }
 
 srun_crb() {
-  limit_total_jobs
   srun="$SRUN --cpus-per-task=7 --nodes=1-1 --ntasks=1 --mem=50G --time=99:00:00"
-  echo "$srun $@"
+  run 1 "$srun $@"
 }
 
 srun_quick() {
-  limit_total_jobs
-  echo "$SRUN --cpus-per-task=1 --nodes=1-1 --ntasks=1 --time=00:10:00 $@"
+  srun="$SRUN --cpus-per-task=1 --nodes=1-1 --ntasks=1 --time=00:10:00"
+  run 1 "$srun $@"
 }
 
 srun_parallel() {
   # monkey-patches a parallel call to run its individual commands via slurm
   # note that it's brittle and only works on shortcut-generated blast commands
-  sleep $(shuf -i 0-300 -n1) # prevent all trying to limit_total_jobs at once
-  limit_total_jobs
+  #sleep $(shuf -i 0-300 -n1) # prevent all trying to limit_total_jobs at once
   cmd="$@"
   before="$(echo "$cmd" | cut -d' ' -f-12)" # ... --pipe
   after="$(echo "$cmd" | cut -d' ' -f13-)"  # '*blast* ...
@@ -57,47 +83,42 @@ srun_parallel() {
   # re-run any affected genomes.
   # TODO can/should I make it fail if one task fails?
   #      (possibly fixed in newer blast+ than 2.2.29?)
-  pargs="-j10 -N1 --delay 0.5" # additional parallel args
+  # pargs="-j50 -N1 --delay 0.2" # additional parallel args
+  pargs="-j20 --block-size 100k --delay 0.2" # additional parallel args
 
   # TODO --exclusive?
   # TODO any chance one will take more than 10 min?
-  srun="$SRUN --cpus-per-task=1 --nodes=1-1 --ntasks=1 --time=00:10:00"
+  srun="$SRUN --cpus-per-task=1 --nodes=1-1 --ntasks=1 --time=99:00:00"
   cmd="${before} ${pargs} ${srun} ${after}"
-  echo "$cmd"
+  run 50 "$cmd"
 }
 
-# If none of the special cases below match, this will run as-is.
-CMD="$@"
 
 # Make parallel blast run individual commands via srun.
 # This one is tricky and may run better with adjustments to the -j parameter.
-if [[ $CMD =~ "--recstart" ]]; then
-  CMD="$(srun_parallel "$CMD")"
+if [[ $@ =~ "--recstart" ]]; then
+  srun_parallel "$@"
 
 # crb-blast spawns parallel jobs itself, so run in a single srun.
 # TODO would this run faster on savio than savio2_htc with more cores?
-elif [[ $CMD == "crb-blast"* ]]; then
-  CMD="$(srun_crb "$CMD")"
+elif [[ $@ == "crb-blast"* ]]; then
+  srun_crb "$@"
 
 # These are quick commands that may be better to run locally depending on the
 # queue. Check `squeue` and remove any that are piling up. Some that seem
 # trivial like `cat` and `cut` can be IO-bound when many run at once on the
 # same machine.
-elif [[ $CMD == md5sum*      ||
-        $CMD == makeblastdb* ||
-        $CMD == blastdbget*  ||
-        $CMD == cut*         ||
-        $CMD == cat*         ||
-        $CMD =~ ".py"        ||
-        $CMD =~ ".R"         ]]; then
-  CMD="$(srun_quick "$CMD")"
+elif [[ $@ == md5sum*      ||
+        $@ == makeblastdb* ||
+        $@ == blastdbget*  ||
+        $@ == cut*         ||
+        $@ == cat*         ||
+        $@ =~ ".py"        ||
+        $@ =~ ".R"         ]]; then
+  srun_quick "$@"
+
+# If nothing else matches run the command locally
+else
+  run 0 "$@"
 
 fi
-
-run() {
- echo "$@"  >> /tmp/wrapper.log
- eval "$@" 2>> /tmp/wrapper.log
-}
-
-# Run the finished command
-run "$CMD"
