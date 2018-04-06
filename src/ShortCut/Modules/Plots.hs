efc01d2e65058787120040297fc07cba64431557
Jeffrey David Johnson <jefdaj@berkeley.edu>
2018-04-06 00:46:04 -0700

    wip: add histogram and scatterplot fns

--- src/ShortCut/Modules/Plots.hs
+++ src/ShortCut/Modules/Plots.hs
@@ -16,7 +16,7 @@ cutModule :: CutModule
 cutModule = CutModule
   { mName = "plots"
   -- , mFunctions = [histogram, linegraph, bargraph, scatterplot, venndiagram]
-  , mFunctions = [histogram]
+  , mFunctions = [histogram, linegraph, scatterplot]
   }
 
 plot :: CutType
@@ -26,19 +26,9 @@ plot = CutType
   , tShow = \_ f -> return $ "png image '" ++ f ++ "'"
   }
 
-linegraph = undefined
-bargraph = undefined
-scatterplot = undefined
-venndiagram = undefined
-
-histogram :: CutFunction
-histogram = let name = "histogram" in CutFunction
-  { fName      = name
-  , fTypeCheck = defaultTypeCheck [str, ListOf num] plot
-  , fTypeDesc  = name ++ " : str num.list -> plot"
-  , fFixity    = Prefix
-  , fRules     = rHistogram
-  }
+---------------
+-- utilities --
+---------------
 
 {- If the user calls a plotting function with a named variable like
  - "num_genomes", this will write that name to a string for use in the plot.
@@ -49,8 +39,12 @@ varName st expr = rLit st $ CutLit str 0 $ case expr of
   (CutRef _ _ _ (CutVar name)) -> name
   _ -> ""
 
-rHistogram :: CutState -> CutExpr -> Rules ExprPath
-rHistogram st@(_,cfg,ref) expr@(CutFun _ _ deps _ [title, nums]) = do
+-- Like varName, but for a list of names
+varNames :: CutState -> [CutExpr] -> Rules ExprPath
+varNames = undefined
+
+rPlot :: FilePath -> CutState -> CutExpr -> Rules ExprPath
+rPlot script st@(_,cfg,ref) expr@(CutFun _ _ _ _ [title, nums]) = do
   titlePath <- rExpr st title
   numsPath  <- rExpr st nums
   xlabPath  <- varName st nums
@@ -59,6 +53,59 @@ rHistogram st@(_,cfg,ref) expr@(CutFun _ _ deps _ [title, nums]) = do
       outPath'' = ExprPath outPath'
       args      = [outPath'', titlePath, numsPath, xlabPath]
       args'     = map (\(ExprPath p) -> toCutPath cfg p) args
-  outPath' %> \_ -> aSimpleScript "histogram.R" cfg ref args'
+  outPath' %> \_ -> aSimpleScript script cfg ref args'
   return outPath''
-rHistogram _ _ = error "bad argument to rHistogram"
+rPlot _ _ _ = error "bad argument to rPlot"
+
+---------------------
+-- plot a num.list --
+---------------------
+
+histogram :: CutFunction
+histogram = let name = "histogram" in CutFunction
+  { fName      = name
+  , fTypeCheck = defaultTypeCheck [str, ListOf num] plot
+  , fTypeDesc  = name ++ " : str num.list -> plot"
+  , fFixity    = Prefix
+  , fRules     = rPlot "histogram.R"
+  }
+
+---------------------
+-- plot num.scores --
+---------------------
+
+tPlotScores :: TypeChecker
+tPlotScores [s, ScoresOf n] | s == str && n == num = Right plot
+tPlotScores _ = Left "expected a title and scores"
+
+-- TODO use varNames instead of varName
+linegraph :: CutFunction
+linegraph = let name = "linegraph" in CutFunction
+  { fName      = name
+  , fTypeCheck = tPlotScores
+  , fTypeDesc  = name ++ " : str num.scores -> plot"
+  , fFixity    = Prefix
+  , fRules     = rPlot "linegraph.R"
+  }
+
+-- TODO use varNames instead of varName
+scatterplot :: CutFunction
+scatterplot = let name = "scatterplot" in CutFunction
+  { fName      = name
+  , fTypeCheck = tPlotScores
+  , fTypeDesc  = name ++ " : str num.scores -> plot"
+  , fFixity    = Prefix
+  , fRules     = rPlot "scatterplot.R"
+  }
+
+----------------------------
+-- plot <whatever>.scores --
+----------------------------
+
+bargraph = undefined
+
+-------------------------------
+-- plot <whatever>.list.list --
+-------------------------------
+
+venndiagram = undefined
---
+++ src/ShortCut/Modules/Plots/scatterplot.R
@@ -0,0 +1,43 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(require(dplyr))
+suppressPackageStartupMessages(require(ggplot2))
+suppressPackageStartupMessages(require(readr))
+
+# TODO read scores and plot two variables here
+
+read_nums <- function(numsPath)
+  read.table(numsPath, col.names=c('score', 'value')) %>% tbl_df
+
+plot_nums <- function(nums, titlePath, xlabPath) {
+  p <- ggplot(data=nums, aes(x=value, y=score)) + geom_point()
+  title <- read_string(titlePath)
+  label <- read_string(xlabPath)
+  if (title != "<<emptystr>>") { p <- p + ggtitle(title) }
+  if (label != "<<emptystr>>") {
+    p <- p + xlab(label)
+  } else {
+    p <- p + xlab("") # remove 'value'
+  }
+  return(p)
+}
+
+read_string <- function(textFile)
+  read_file(textFile) %>% trimws
+
+save_plot <- function(plot, plotPath)
+  ggsave(plot, filename=plotPath)
+
+main <- function() {
+  # args <- c('testplot.png', 'testtitle.txt', 'testnums.txt', 'testxlab.txt')
+  args <- commandArgs(trailingOnly = TRUE)
+  plotPath  <- args[[1]]
+  titlePath <- args[[2]] # might contain "<<emptystr>>"
+  numsPath  <- args[[3]]
+  xlabPath  <- args[[4]] # might contain "<<emptystr>>"
+  read_nums(numsPath) %>%
+    plot_nums(titlePath, xlabPath) %>%
+    save_plot(plotPath)
+}
+
+main()
