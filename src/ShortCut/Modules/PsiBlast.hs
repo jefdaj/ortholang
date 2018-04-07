c95ce8be2f246bfbc2fe3b90e565c10f360c4c0c
Jeffrey David Johnson <jefdaj@berkeley.edu>
2018-04-06 20:40:53 -0700

    skeleton psiblast_train fns

--- src/ShortCut/Modules/PsiBlast.hs
+++ src/ShortCut/Modules/PsiBlast.hs
@@ -4,13 +4,21 @@ module ShortCut.Modules.PsiBlast where
  - faster. I haven't compared them directly yet, but it does seem fast.
  -}
 
-import Development.Shake
+-- import Development.Shake
 import ShortCut.Core.Types
+
+import ShortCut.Core.Compile.Basic (defaultTypeCheck)
+import ShortCut.Modules.BlastDB    (pdb)
+import ShortCut.Modules.SeqIO      (faa)
 
 cutModule :: CutModule
 cutModule = CutModule
   { mName = "psiblast"
-  , mFunctions = []
+  , mFunctions =
+    [ psiblastTrain
+    , psiblastTrainAll
+    , psiblastTrainDb
+    ]
   }
 
 -- TODO checkpoint PSSM type? not unless needed
@@ -18,13 +26,47 @@ cutModule = CutModule
 pssm :: CutType
 pssm = CutType
   { tExt  = "pssm"
-  , tDesc = "PSI-BLAST position-specific substitution matrix (ASCII)"
+  , tDesc = "PSI-BLAST position-specific substitution matrix as ASCII"
   , tShow  = defaultShow
   }
 
--- TODO psiblast_train        : faa  faa      -> pssm
--- TODO psiblast_train_all    : faa  faa.list -> pssm
--- TODO psiblast_train_db     : faa  pdb      -> pssm
+-----------------
+-- train pssms --
+-----------------
+
+psiblastTrainDb :: CutFunction
+psiblastTrainDb = CutFunction
+  { fName      = name
+  , fTypeCheck = defaultTypeCheck [faa, pdb] pssm
+  , fTypeDesc  = mkTypeDesc name  [faa, pdb] pssm
+  , fFixity    = Prefix
+  , fRules     = undefined
+  }
+  where
+    name = "psiblast_train_db"
+
+psiblastTrain :: CutFunction
+psiblastTrain = CutFunction
+  { fName      = name
+  , fTypeCheck = defaultTypeCheck [faa, faa] pssm
+  , fTypeDesc  = mkTypeDesc name  [faa, faa] pssm
+  , fFixity    = Prefix
+  , fRules     = undefined
+  }
+  where
+    name = "psiblast_train"
+
+psiblastTrainAll :: CutFunction
+psiblastTrainAll = CutFunction
+  { fName      = name
+  , fTypeCheck = defaultTypeCheck [faa, ListOf faa] pssm
+  , fTypeDesc  = mkTypeDesc name  [faa, ListOf faa] pssm
+  , fFixity    = Prefix
+  , fRules     = undefined
+  }
+  where
+    name = "psiblast_train_all"
+
 -- TODO psiblast              : faa  faa      -> bht
 -- TODO psiblast_all          : faa  faa.list -> bht
 -- TODO psiblast_db           : faa  pdb      -> bht
