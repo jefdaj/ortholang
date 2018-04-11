f010fe0dea17e70a8cfb8f32e7721d7bb773437d
Jeffrey David Johnson <jefdaj@berkeley.edu>
2018-04-11 08:32:19 -0700

    wip: psiblast fns compile with undefined map3of4

--- src/ShortCut/Core/Types.hs
+++ src/ShortCut/Core/Types.hs
@@ -4,6 +4,7 @@ module ShortCut.Core.Types
   , Action1
   , Action2
   , Action3
+  , Action4
   , ActionFn
   , RulesFn
   , TypeChecker
@@ -83,6 +84,7 @@ newtype CutPath = CutPath FilePath deriving (Eq, Ord, Show)
 type Action1 = CutConfig -> Locks -> CutPath -> Action ()
 type Action2 = CutConfig -> Locks -> CutPath -> CutPath -> Action ()
 type Action3 = CutConfig -> Locks -> CutPath -> CutPath -> CutPath -> Action ()
+type Action4 = CutConfig -> Locks -> CutPath -> CutPath -> CutPath -> CutPath -> Action ()
 
 -- TODO remove when able in favor of well-typed versions above
 type ActionFn    = CutConfig -> CacheDir -> [ExprPath] -> Action ()
