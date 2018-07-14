aff690c4ca2c60cb2926c2497e133a428dab9efa
Jeffrey David Johnson <jefdaj@berkeley.edu>
2018-07-13 18:12:45 -0700

    add shake log abbreviations

--- src/ShortCut/Core/Eval.hs
+++ src/ShortCut/Core/Eval.hs
@@ -55,6 +55,7 @@ myShake cfg rules = do
       , shakeVerbosity = if cfgDebug cfg then Chatty else Quiet
       , shakeThreads   = 0 -- TODO make customizable? increase?
       , shakeReport    = [cfgTmpDir cfg </> "profile.html"] ++ (maybeToList $ cfgReport cfg)
+      , shakeAbbreviations = [(cfgTmpDir cfg, "$TMPDIR"), (cfgWorkDir cfg, "$WORKDIR")]
       -- , shakeChange    = ChangeModtimeAndDigest -- TODO test this
       -- , shakeCommandOptions = [EchoStdout True]
       -- , shakeProgress = progressSimple
