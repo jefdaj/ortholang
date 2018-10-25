e49eec5c82ed381a994deba768ee9b03a39e0d49
Jeffrey David Johnson <jefdaj@berkeley.edu>
2018-10-25 01:32:48 +0000

    skeleton reference cabal target

---
+++ Reference.hs
@@ -0,0 +1,17 @@
+module Main where
+
+-- import Paths_ShortCut        (version)
+-- import ShortCut.Core         (runRepl, evalFile)
+-- import ShortCut.Core.Types   (CutConfig(..))
+
+-- generate reference page:
+--   cfgModules cfg -> list of modules
+--   for each module:
+--     first print the list with markdown sections
+--     add a general description of the module here
+--     then go through the functions:
+--       mFunctions mod -> fName etc to get info
+
+main :: IO ()
+main = do
+  undefined
