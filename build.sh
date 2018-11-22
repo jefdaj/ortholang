7bbb5a2b8ddaa58183725b9456dcd2ae915f390f
Jeffrey David Johnson <jefdaj@berkeley.edu>
2018-11-22 10:51:38 -0800

    update build.sh

--- build.sh
+++ build.sh
@@ -7,17 +7,18 @@
 export TASTY_QUICKCHECK_TESTS=1000
 export TASTY_COLOR="always"
 export TASTY_QUICKCHECK_SHOW_REPLAY=True
-export TASTY_HIDE_SUCCESSES=True
+export TASTY_HIDE_SUCCESSES=False
 [[ -z "$TMPDIR" ]] && export TMPDIR=/tmp
+TEST_ARGS="--test $@ +RTS -IO -N -RTS"
 
 # this builds everything at once, which is simpler.
 # the downside is it rebuilds the haskell code from scratch.
 echo "testing nix build..."
-(nix-build -j$(nproc) && ./result/bin/shortcut --test --pattern output $@ +RTS -IO -N -RTS) 2>&1 | tee nix-build.log
+(nix-build -j$(nproc) && ./result/bin/shortcut $TEST_ARGS) 2>&1 | tee nix-build.log
 
 # this does an incremental build of the haskell code for faster testing
 echo "testing stack build..."
 export STACK_ROOT=$PWD/.stack-work
 mkdir -p $STACK_ROOT
-cmd='(stack build --allow-different-user && ./.stack-work/install/*/*/*/bin/shortcut --test) || exit'
+cmd='(stack build --allow-different-user && ./.stack-work/install/*/*/*/bin/shortcut $TEST_ARGS) || exit'
 nix-shell --command "$cmd" 2>&1 | tee stack-build.log
