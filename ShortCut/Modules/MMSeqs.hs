77da5a6a14b1f4499490a5be236255f542c8ee15
Jeffrey David Johnson <jefdaj@berkeley.edu>
2018-11-26 02:43:45 +0000

    todos

--- ShortCut/Modules/MMSeqs.hs
+++ ShortCut/Modules/MMSeqs.hs
@@ -10,7 +10,11 @@ cutModule = CutModule
   , mTypes = []
   , mFunctions =
       [ 
+      -- TODO mmseqs_createdb : fa -> mms
+      -- TODO mmseqs_search_all : fa.list mms -> mmr (this is mmseqs search)
+      -- TODO mmseqs_search : fa mms -> mmr (this is mmseqs search with a singleton list)
       ]
   }
 
-
+-- TODO mms type for mmseqs subject (or sequence?) db?
+-- TODO mmh type for mmseqs result/hits db?
