45ae6b11c6268eb754bb2e7c67992991f39f3b19
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-01-06 22:23:49 +0000

    wip: add a length fn stub for mmseqs dbs

--- Detourrr/Modules/MMSeqs.hs
+++ Detourrr/Modules/MMSeqs.hs
@@ -3,6 +3,7 @@ module Detourrr.Modules.MMSeqs
 
 -- TODO keep intermediate files at least until we can line them up with sonicparanoid if needed
 -- TODO default to the same settings sonicparanoid uses at first, both to be compatible and until you know better
+-- TODO length function should be polymorphic enough to also do dbs
 
 -- TODO mmseqs_search_all : fa.list mms -> mmr (this is mmseqs search)
 -- TODO mmseqs_search_profile?
@@ -39,6 +40,7 @@ dtrModule = DtrModule
       , mmseqsCreateDb
       , mmseqsSearchDb
       , mmseqsSearch
+      , mmseqsDbLength -- TODO write this, TODO unify with length
       ]
   }
 
@@ -145,7 +147,7 @@ rMmseqsSearchDb st@(_, cfg, ref, _) e@(DtrFun _ salt _ _ [n, q, s]) = do
       dbDir  = cfgTmpDir cfg </> "cache" </> "mmseqs" </> "createdb" -- TODO be more or less specific?
       -- outDb' = dbDir </> "db" -- TODO error here?
       outDb' = dbDir </> digest out <.> "mmseqs2db" -- TODO does this hash match the ones from createdb_all?
-  outDb' %> \_ -> aMmseqSearchDb    cfg ref ePath qPath sPath outDb'
+  outDb' %> \_ -> aMmseqsSearchDb    cfg ref ePath qPath sPath outDb'
   out'   %> \_ -> aMmseqConvertAlis cfg ref       qPath sPath outDb' out'
   return (ExprPath out')
 rMmseqsSearchDb _ e = error $ "bad argument to rMmseqsSearch: " ++ show e
@@ -155,11 +157,13 @@ resolveMmseqsDb path = do
   need [path] -- path is to a symlink to the main db file
   liftIO $ resolveSymlinks Nothing path
 
-aMmseqSearchDb :: DtrConfig -> Locks -> FilePath -> FilePath -> FilePath -> FilePath -> Action ()
-aMmseqSearchDb cfg ref ePath qDb sDb outDb = do
+-- TODO is the db being passed in place of the fa too?
+-- TODO interesting! it works in stack repl but not stack exec
+aMmseqsSearchDb :: DtrConfig -> Locks -> FilePath -> FilePath -> FilePath -> FilePath -> Action ()
+aMmseqsSearchDb cfg ref ePath qDb sDb outDb = do
+  eStr <- readLit cfg ref ePath
   qDb' <- resolveMmseqsDb qDb
   sDb' <- resolveMmseqsDb sDb
-  eStr <- readLit cfg ref ePath
   let tmpDir = takeDirectory outDb </> "tmp" -- TODO align this with sonicparanoid
   liftIO $ createDirectoryIfMissing True tmpDir
   wrappedCmdWrite True True cfg ref outDb [ePath, qDb, sDb] [] []
@@ -202,5 +206,22 @@ rMmseqsSearch st (DtrFun rtn salt deps name [e, q, s])
   = rules st (DtrFun rtn salt deps name [e, q, sDb])
   where
     rules = fRules mmseqsSearchDb
-    sDb = DtrFun mms salt (depsOf s) "mmseqs_createdb" [s] 
+    sDb = DtrFun mms salt (depsOf s) "mmseqs_createdb" [s] -- TODO also accept a fa.list here?
 rMmseqsSearch _ _ = error "bad argument to rMmseqsSearch"
+
+---------------------
+-- mmseqs_dblength --
+---------------------
+
+mmseqsDbLength :: DtrFunction
+mmseqsDbLength = let name = "mmseqs_dblength" in DtrFunction
+  { fName      = name
+  , fTypeDesc  = name ++ " : mms -> num"
+  , fTypeCheck = tMmseqsSearch name
+  , fDesc      = Just "Get the number of sequences (or overall size?) of an MMseqs2 database"
+  , fFixity    = Prefix
+  , fRules     = rMmseqsDbLength
+  }
+
+rMmseqsDbLength :: RulesFn
+rMmseqsDbLength = undefined
