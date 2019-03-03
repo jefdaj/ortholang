fe4280c71b9b52db7e9bae0d756f119d558d3e11
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-03-03 12:31:39 -0800

    got one shell script test working

--- ShortCut/Test/Scripts.hs
+++ ShortCut/Test/Scripts.hs
@@ -1,11 +1,13 @@
 module ShortCut.Test.Scripts where
 
+-- import Debug.Trace (trace)
+
 import Prelude hiding (writeFile)
 
 import Control.Concurrent.Thread.Delay (delay)
 import Control.Monad              (when)
 import Data.ByteString.Lazy.Char8 (pack, ByteString)
-import Data.List                  (isPrefixOf)
+import Data.List                  (zip4)
 import Data.Maybe                 (fromJust)
 import Paths_ShortCut             (getDataFileName)
 import ShortCut.Core.Eval         (evalFile)
@@ -28,13 +30,13 @@ import Test.Tasty.Golden          (goldenVsStringDiff, findByExtension)
 import Test.Tasty.Hspec           (testSpecs, shouldReturn)
 
 -- TODO remove in favor of shell scripts that test specific properties of tmpfiles
-nonDeterministicCut :: FilePath -> Bool
-nonDeterministicCut path = any (\s -> s `isPrefixOf` (takeBaseName path)) badSigns
-  where
-    -- TODO will regular blast be nondeterministic at large scales too?
-    -- TODO make these individual tests, or substrings
-    -- TODO eventually replace deterministic or not with custom shell predicates
-    badSigns = ["crb_blast", "blastrbh", "blasthits", "plot", "mmseqs"] -- TODO blast? blastdb?
+-- nonDeterministicCut :: FilePath -> Bool
+-- nonDeterministicCut path = any (\s -> s `isPrefixOf` (takeBaseName path)) badSigns
+--   where
+--     -- TODO will regular blast be nondeterministic at large scales too?
+--     -- TODO make these individual tests, or substrings
+--     -- TODO eventually replace deterministic or not with custom shell predicates
+--     badSigns = ["crb_blast", "blastrbh", "blasthits", "plot", "mmseqs"] -- TODO blast? blastdb?
 
 knownFailing :: [FilePath]
 knownFailing =
@@ -72,8 +74,8 @@ knownFailing =
 
 getTestScripts :: IO [FilePath]
 getTestScripts = do
-  testDir  <- getDataFileName "tests"
-  testCuts <- findByExtension [".cut"] testDir -- TODO remove known failing scripts here
+  testDir  <- getDataFileName "tests/scripts"
+  testCuts <- findByExtension [".cut"] testDir
   let testCuts' = filter (\p -> not $ (takeBaseName p) `elem` knownFailing) testCuts
   return testCuts'
 
@@ -83,13 +85,13 @@ goldenDiff name file action = goldenVsStringDiff name fn file action
     -- this is taken from the Tasty docs
     fn ref new = ["diff", "-u", ref, new]
 
+-- TODO use <testdir>/output.txt instead of the raw output?
 mkOutTest :: CutConfig -> Locks -> HashedSeqIDsRef -> FilePath -> TestTree
 mkOutTest cfg ref ids gld = goldenDiff desc gld scriptAct
   where
     -- TODO put toGeneric back here? or avoid paths in output altogether?
     scriptAct = do
       out <- runCut cfg ref ids
-      -- writeFile ("/tmp" </> takeBaseName gld <.> "txt") out
       return $ pack out
     desc = "prints expected output"
 
@@ -100,7 +102,7 @@ mkTreeTest cfg ref ids t = goldenDiff desc t treeAct
     -- TODO refactor them to come from the same fn
     desc = "creates expected tmpfiles"
     sedCmd  = "sed 's/lines\\/.*/lines\\/\\.\\.\\./g'"
-    treeCmd = (shell $ "tree -aI '*.lock|*.database|*.log|*.tmp|*.html|lines' | " ++ sedCmd)
+    treeCmd = (shell $ "tree -aI '*.lock|*.database|*.log|*.tmp|*.html|lines|output.txt' | " ++ sedCmd)
                 { cwd = Just $ cfgTmpDir cfg }
     treeAct = do
       _ <- runCut cfg ref ids
@@ -139,6 +141,9 @@ mkAbsTest cfg ref ids = testSpecs $ it desc $
       (_, out, err) <- readProcessWithExitCode "grep" absArgs ""
       return $ toGeneric cfg $ out ++ err
 
+{- This is more or less idempotent because re-running the same cut multiple
+ - times is fast. So it's OK to run it once for each test in a group.
+ -}
 runCut :: CutConfig -> Locks -> HashedSeqIDsRef -> IO String
 runCut cfg ref ids =  do
   delay 100000 -- wait 0.1 second so we don't capture output from tasty (TODO is that long enough?)
@@ -146,34 +151,58 @@ runCut cfg ref ids =  do
   delay 100000 -- wait 0.1 second so we don't capture output from tasty (TODO is that long enough?)
   result <- doesFileExist $ cfgTmpDir cfg </> "vars" </> "result"
   when (not result) (fail out)
+  writeFile (cfgTmpDir cfg </> "output" <.> "txt") $ toGeneric cfg out -- for the shell script tests
   return $ toGeneric cfg out
 
-mkScriptTests :: (FilePath, FilePath, Maybe FilePath)
+mkScriptTests :: (FilePath, Maybe FilePath, Maybe FilePath, Maybe FilePath)
               -> CutConfig -> Locks -> HashedSeqIDsRef -> IO TestTree
-mkScriptTests (cut, gld, mtre) cfg ref ids = do
-  absTests <- mkAbsTest cfg' ref ids -- just one, but comes as a list
-  return $ testGroup name $ otherTests ++ absTests
+mkScriptTests (cut, mout, mtre, mshell) cfg ref ids = do
+  absTests   <- mkAbsTest   cfg' ref ids -- just one, but comes as a list
+  shellTests <- case mshell of
+                  Just s  -> mkCheckTest cfg' ref ids s
+                  Nothing -> return []
+  let tripTest   = mkTripTest cfg' ref ids
+      otherTests = [tripTest] ++ treeTests ++ shellTests ++ outTests
+      outTests   = case mout of
+                     Just o  -> [mkOutTest cfg' ref ids o]
+                     Nothing -> []
+      treeTests  = case mtre of
+                     Just t  -> [mkTreeTest cfg' ref ids t]
+                     Nothing -> []
+  return $ testGroup name $ otherTests ++ absTests -- ++ shellTests
   where
-    name       = takeFileName cut
-    cfg'       = cfg { cfgScript = Just cut, cfgTmpDir = (cfgTmpDir cfg </> name) }
-    otherTests = [mkTripTest cfg' ref ids, mkOutTest cfg' ref ids gld] ++ genTests
-    genTests   = case mtre of
-                   Just t  -> [mkTreeTest cfg' ref ids t]
-                   Nothing -> []
+    name = takeFileName cut
+    cfg' = cfg { cfgScript = Just cut, cfgTmpDir = (cfgTmpDir cfg </> name) }
+
+{- "check scripts" for handling the tricky cases:
+ - they get passed the tmpdir as their only argument
+ - they should give no output if the tests pass, and print errors otherwise
+ - TODO move stdout inside the tmpdir?
+ -}
+mkCheckTest :: CutConfig -> Locks -> HashedSeqIDsRef -> FilePath -> IO [TestTree]
+mkCheckTest cfg ref ids scr = testSpecs $ it desc $ runCheck `shouldReturn` ""
+  where
+    desc = "output + tmpfiles checked by script"
+    runCheck = do
+      _ <- runCut cfg ref ids
+      (_, out, err) <- readProcessWithExitCode "bash" [scr, cfgTmpDir cfg] ""
+      return $ toGeneric cfg $ out ++ err
+
+findTestFile :: String -> String -> FilePath -> IO (Maybe FilePath)
+findTestFile dir ext cut = do
+  exists <- doesFileExist testPath
+  return $ if exists then Just testPath else Nothing
+  where
+    testDir  = init $ init $ splitDirectories cut
+    testPath = joinPath $ testDir ++ [dir, replaceExtension (takeBaseName cut) ext]
 
 mkTests :: CutConfig -> Locks -> HashedSeqIDsRef -> IO TestTree
 mkTests cfg ref ids = do
-  cuts <- getTestScripts
-  let outs     = map findOutFile  cuts
-      mtrees   = map findTreeFile cuts
-      triples  = zip3 cuts outs mtrees
-      groups   = map mkScriptTests triples
+  cuts    <- getTestScripts
+  mouts   <- mapM (findTestFile "stdout"   "txt") cuts
+  mtrees  <- mapM (findTestFile "tmpfiles" "txt") cuts
+  mchecks <- mapM (findTestFile "check"    "sh" ) cuts
+  let quads  = zip4 cuts mouts mtrees mchecks
+      -- groups = map mkScriptTests $ trace ("quads: " ++ show quads) quads
+      groups = map mkScriptTests quads
   mkTestGroup cfg ref ids "interpret test scripts" groups
-  where
-    -- findOutFile  c = takeDirectory c </> replaceExtension c "txt"
-    findOutFile c = joinPath $ (init $ init $ splitDirectories c)
-                      ++ ["stdout", replaceExtension (takeBaseName c) "txt"]
-    findTreeFile c = if nonDeterministicCut c
-      then Nothing
-      else Just $ joinPath $ (init $ init $ splitDirectories c)
-                    ++ ["tmpfiles", replaceExtension (takeBaseName c) "txt"]
