506d4c41487264a57abdd729831b283f8f81134f
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-03-04 22:43:40 -0800

    document Replace.hs in preparation for rewrite

--- ShortCut/Core/Compile/Replace.hs
+++ ShortCut/Core/Compile/Replace.hs
@@ -1,31 +1,65 @@
 module ShortCut.Core.Compile.Replace where
 
--- TODO is an initial var to start from needed when doing replace, or can that be the whole script?
-
-{- Why can't we use an obvious var mangling scheme again?
- -   vars/<varname>.<salt>.<ext>
- -   vars/<salt>/<varname>.<ext>
- -   vars/<varname>/<varname>.<salt>.<ext>
- - Maybe just have to delete all var links when starting?
- - Start by moving to prefix dirs, then rename to ints if possible
+{- This is some pretty awkward code, but I also think it's one of the coolest
+ - features of ShortCut! It conveniently answers the common question "What
+ - would happen to my results if I changed variable X?" You can already edit
+ - and re-run any code of course, but it tends to be something people don't
+ - take good notes on. Having it built in to the language both documents and
+ - parallelizes everything. It also makes it easy to plot the results so you
+ - don't have to write your own R/python script for that (until you want
+ - publication-quality figures).
+ -
+ - It's implemented by:
+ - 1) copying the whole script once per variable substitution
+ - 2) "mangling" the file names in the copies so their variables don't conflict with existing ones
+ - 3) gathering a list of the final results into one final variable in the main namespace
+ -
+ - The mangling thing is ugly, but relatively simple to implement and works well.
+ - It results in a tree of "rep" symlinks like this:
+ -
+ - reps
+ -    ├── 312356c109
+ -    │   ├── dep.num -> ../../exprs/multiply/653e971b5e_cd4761ce86_0.num
+ -    │   ├── ind.num -> ../../exprs/num/89fb8b4aed_0.num
+ -    │   └── result -> ../../reps/312356c109/dep.num
+ -    ├── 4792af49ff
+ -    │   ├── dep.num -> ../../exprs/multiply/653e971b5e_698967e992_0.num
+ -    │   └── result -> ../../reps/4792af49ff/dep.num
+ -    ├── 4ab901304c
+ -    │   ├── dep.num -> ../../exprs/multiply/653e971b5e_538f7a4f17_0.num
+ -    │   ├── ind.num -> ../../exprs/num/a629005e3c_0.num
+ -    │   └── result -> ../../reps/4ab901304c/dep.num
+ -    ├── 6eaa4d790a
+ -    │   ├── dep.num -> ../../exprs/multiply/653e971b5e_e05aae5b00_0.num
+ -    │   ├── ind.num -> ../../exprs/num/eaad7f2e40_0.num
+ -    │   └── result -> ../../reps/6eaa4d790a/dep.num
+ -    └── e8c54e2b5c
+ -        ├── dep.num -> ../../exprs/multiply/653e971b5e_adf56c2208_0.num
+ -        ├── ind.num -> ../../exprs/num/ea11b49459_0.num
+ -        └── result -> ../../reps/e8c54e2b5c/dep.num
+ -
+ -TODO is an initial var to start from really needed, or can that be the whole script?
  -}
 
 import Development.Shake
 import ShortCut.Core.Types
 
-import ShortCut.Core.Actions (writeLits, writePaths, readLit, debugA)
-import ShortCut.Core.Paths  (exprPath, fromCutPath,
-                             CutPath, toCutPath, fromCutPath)
+import ShortCut.Core.Actions       (writeLits, writePaths, readLit, debugA)
+import ShortCut.Core.Paths         (exprPath, fromCutPath,
+                                    CutPath, toCutPath, fromCutPath)
 import ShortCut.Core.Compile.Basic (rExpr, compileScript, debugRules)
--- import ShortCut.Core.Debug   (debugRules, debugA)
-import ShortCut.Core.Util    (digest, stripWhiteSpace)
+import ShortCut.Core.Util          (digest, stripWhiteSpace)
 
---------------------------------------------------------
--- prefix variable names so duplicates don't conflict --
---------------------------------------------------------
+---------------------------
+-- mangle variable names --
+---------------------------
 
--- TODO only mangle the specific vars we want changed!
--- TODO can all this silliness be avoided using random salts?
+{- This does the filename mangling by setting a "replace ID" in each variable
+ - in a script. If it's anything other than Nothing it gets used by
+ - ShortCut.Core.Paths.varPath to set the rep dir.
+ -
+ - TODO should it go in Types, or maybe Paths?
+ -}
 
 mangleExpr :: (CutVar -> CutVar) -> CutExpr -> CutExpr
 mangleExpr _ e@(CutLit  _ _ _) = e
@@ -41,33 +75,28 @@ mangleAssign fn (var, expr) = (fn var, mangleExpr fn expr)
 mangleScript :: (CutVar -> CutVar) -> CutScript -> CutScript
 mangleScript fn = map (mangleAssign fn)
 
--- TODO should this all go in Types?
 setReplaceID :: ReplaceID -> CutVar -> CutVar
 setReplaceID newID (CutVar _ name) = CutVar newID name
 
--- TODO pad with zeros?
--- Add a "dupN." prefix to each variable name in the path from independent
--- -> dependent variable, using a list of those varnames
--- changeReplaceID :: String -> (CutVar -> CutVar)
--- changeReplaceID p (CutVar (ReplaceID Nothing)  s) = CutVar (ReplaceID $ Just p) s
--- changeReplaceID p (CutVar (ReplaceID (Just r)) s) = CutVar (ReplaceID $ Just $ digest $ p ++ r) s
--- changeReplaceID salt (CutVar _ s) = CutVar (ReplaceID salt) s
-
--- TODO should be able to just apply this to a duplicate script section right?
 setReplaceIDs :: ReplaceID -> CutScript -> CutScript
 setReplaceIDs newID = mangleScript (setReplaceID newID)
 
------------------------------------
--- main replace function for PRS --
------------------------------------
+---------------
+-- interface --
+---------------
+
+{- Standard ShortCut boilerplate to create user-facing functions.
+ - TODO plain replace function that replaces one variable. easy to implement with a singleton
+ -}
 
 replaceEach :: CutFunction
 replaceEach = CutFunction
   { fName      = "replace_each"
   , fFixity    = Prefix
   , fTypeCheck = tReplaceEach
-  , fDesc = Nothing, fTypeDesc  = dReplaceEach
-  , fRules  = rReplaceEach
+  , fDesc      = Nothing
+  , fTypeDesc  = dReplaceEach
+  , fRules     = rReplaceEach
   }
 
 tReplaceEach :: [CutType] -> Either String CutType
@@ -77,40 +106,28 @@ tReplaceEach _ = Left "invalid args to replace_each" -- TODO better errors here
 dReplaceEach :: String
 dReplaceEach = "replace_each : <outputvar> <inputvar> <inputvars> -> <output>.list"
 
--- TODO how unique should this be?? want as much deduplication as possible while staying safe
-calcReplaceID :: CutState -> CutExpr -> CutVar -> CutExpr -> ReplaceID
-calcReplaceID (scr, _, _, _) resExpr subVar subExpr = ReplaceID $ Just $ digest
-  [ show scr
-  , show resExpr
-  , show subVar
-  , show subExpr
-  ]
+--------------------
+-- implementation --
+--------------------
 
--- TODO ideally, this shouldn't need any custom digesting? but whatever no
---      messing with it for now
--- TODO can this be parallelized better?
-cReplace :: CutState -> CutExpr -> CutVar -> CutExpr -> Rules ExprPath
-cReplace st@(script, cfg, ref, ids) resExpr subVar@(CutVar _ _) subExpr = do
-  let res  = (CutVar (ReplaceID Nothing) "result", resExpr) -- TODO is this the right salt?
-      sub  = (subVar, subExpr)
-      deps = filter (\(v,_) -> (elem v $ depsOf resExpr ++ depsOf subExpr)) script
-      -- TODO this should be very different right?
-      -- pre  = digest $ map show $ res:sub:deps
-      -- (ReplaceID s) = saltOf resExpr
-             -- Just r  -> digest $ ReplaceID $ show $ fst $ next $ (read r :: StdGen)
-      -- newID = case rep of
-      --           Nothing -> fail "bad argument to cReplace (no ReplaceID)"
-      --           Just r  -> r
-      newID = calcReplaceID st resExpr subVar subExpr
-      scr' = (setReplaceIDs newID ([sub] ++ deps ++ [res]))
-  (ResPath resPath) <- compileScript (scr', cfg, ref, ids) newID -- TODO remove the ID here, or is it useful?
-  return (ExprPath resPath) -- TODO this is supposed to convert result -> expr right?
-
-rReplaceEach :: CutState -> CutExpr -> Rules ExprPath
+{- This takes a list expression that includes the variable to replace and a
+ - list of things to replace it with. It calls rReplace to do each replacement
+ - operation, then gathers the results in a list.
+ -
+ - It has one major flaw that I'm trying to fix now: it assumes that the list
+ - of expressions to substitute in are known at compile-time. That makes it
+ - impossible to repeat based on the results of a function call, limiting it to
+ - just lists you explicitly write in the cut code.
+ -
+ - TODO plan the rewrite in detail, or at least try to
+ -}
+rReplaceEach :: CutState
+             -> CutExpr -- the final result expression, which contains all the info we need
+             -> Rules ExprPath
 rReplaceEach s@(scr, cfg, ref, _) expr@(CutFun _ _ _ _ (resExpr:(CutRef _ _ _ subVar):subList:[])) = do
   subPaths <- rExpr s subList
   let subExprs = extractExprs scr subList
-  resPaths <- mapM (cReplace s resExpr subVar) subExprs
+  resPaths <- mapM (rReplace s resExpr subVar) subExprs
   let subPaths' = (\(ExprPath p) -> toCutPath cfg p) subPaths
       resPaths' = map (\(ExprPath p) -> toCutPath cfg p) resPaths
       outPath   = exprPath s expr
@@ -123,8 +140,52 @@ rReplaceEach s@(scr, cfg, ref, _) expr@(CutFun _ _ _ _ (resExpr:(CutRef _ _ _ su
   return (ExprPath outPath')
 rReplaceEach _ expr = error $ "bad argument to rReplaceEach: " ++ show expr
 
--- TODO factor out, and maybe unify with rListLits
--- TODO subPaths is only one path? if so, rename it
+{- This does the actual replace operation. It takes an expression to edit, the
+ - variable to replace, and an expression to put in its place. While it does
+ - that it also filters to remove any irrelevant variables and sets a unique
+ - "replace ID" on everything to keep the new copies of variables from
+ - conflicting with the existing ones. The copies go in a separate "reps"
+ - folder named by the ID.
+ -
+ - TODO can this be wrapped and exported as-is to be the replace function?
+ -      not quite, but if you add something to take a single expression that should work
+ - TODO ideally, this shouldn't need any custom digesting? but whatever no
+ -      messing with it for now
+ - TODO can this be parallelized better?
+ -}
+rReplace :: CutState
+         -> CutExpr -- the result expression for a single replacement, which *doesn't* contain all the info
+         -> CutVar  -- we also need the variable to be replaced
+         -> CutExpr -- and an expression to replace it with (which could be a ref to another variable)
+         -> Rules ExprPath
+rReplace st@(script, cfg, ref, ids) resExpr subVar@(CutVar _ _) subExpr = do
+  let res   = (CutVar (ReplaceID Nothing) "result", resExpr)
+      sub   = (subVar, subExpr)
+      deps  = filter (\(v,_) -> (elem v $ depsOf resExpr ++ depsOf subExpr)) script
+      newID = calcReplaceID st resExpr subVar subExpr
+      scr'  = (setReplaceIDs newID ([sub] ++ deps ++ [res]))
+  (ResPath resPath) <- compileScript (scr', cfg, ref, ids) newID -- TODO remove the ID here, or is it useful?
+  return (ExprPath resPath)
+
+{- This decides the "replace ID" in rReplace above. It's important because the
+ - hash needs to be unique whenever we would want to return different results,
+ - but the same between things that we actually want deduplicated. So far we
+ - err on the side of uniqueness.
+ -
+ - TODO think carefully about whether all of these need to be in here
+ -}
+calcReplaceID :: CutState -> CutExpr -> CutVar -> CutExpr -> ReplaceID
+calcReplaceID (scr, _, _, _) resExpr subVar subExpr = ReplaceID $ Just $ digest
+  [ show scr
+  , show resExpr
+  , show subVar
+  , show subExpr
+  ]
+
+{- Helper function to write the final list when the results are literals
+ - TODO factor out, and maybe unify with rListLits
+ - TODO subPaths is only one path? if so, rename it
+ -}
 aReplaceEachLits :: CutType -> CutConfig -> Locks
                 -> CutPath -> CutPath -> [CutPath] -> Action ()
 aReplaceEachLits _ cfg ref outPath subPaths resPaths = do
@@ -137,7 +198,9 @@ aReplaceEachLits _ cfg ref outPath subPaths resPaths = do
     resPaths' = map (fromCutPath cfg) resPaths
     out = debugA cfg "aReplaceEachLits" outPath' (outPath':subPaths':resPaths')
 
--- TODO factor out, and maybe unify with rListLinks
+{- Helper function to write the final list when the results are links to files
+ - TODO factor out, and maybe unify with rListLinks
+ -}
 aReplaceEachLinks :: CutConfig -> Locks -> CutPath -> CutPath -> [CutPath] -> Action ()
 aReplaceEachLinks cfg ref outPath subPaths resPaths = do
   need (subPaths':resPaths') -- TODO is needing subPaths required?
