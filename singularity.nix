ae31392c8338057a4bc0bb6fcc3df6d25f50af1f
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-03-20 13:25:15 -0700

    got singularity bind dirs working on hpc (requires my nixpkgs)

--- singularity.nix
+++ singularity.nix
@@ -4,7 +4,17 @@ let shortcut = import ./default.nix;
 
 in pkgs.singularity-tools.buildImage {
   name = "shortcut-${shortcut.version}";
-  contents = [ shortcut ];
-  runScript = "${shortcut}/bin/shortcut";
-  diskSize = 10240; # TODO can this be smaller?
+  contents = [ binutils shortcut ];
+
+  # Seems to work better if I don't mess with this. Instead, run shortcut
+  # (or any of its runtime dependencies) from bash inside the container.
+  # runScript = "#!${stdenv.shell}\nexec /bin/sh";
+
+  diskSize = 10240;
+  # TODO is there a way to have these auto-bind when running the image too?
+  # these should be absolute paths but missing the leading slash:
+  bindDirs = [
+    "global/home/users"
+    "global/scratch"
+  ];
 }
