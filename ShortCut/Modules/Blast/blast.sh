550c5f2cdaf82976cfa0b61e0ea9947f1741e617
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-05-13 17:17:57 -0700

    wip on blast script integration

--- ShortCut/Modules/Blast/blast.sh
+++ ShortCut/Modules/Blast/blast.sh
@@ -3,19 +3,20 @@
 set -e
 
 OUTPATH="$1"
-DBPATH="$2"
+DBPATH="$2" # same as prefix in the haskell code?
 BLASTCMD="$3"
-BLASTARGS="$4"
-EDEC="$5" # this is the actual evalue, not a path
-QPATH="$6"
-PPATH="$7" # TODO why not QPATH?
+EDEC="$4" # this is the actual evalue, not a path
+QPATH="$5"
+PPATH="$6" # TODO why not QPATH?
 
-export BLASTDB="$(dirname "$DBPATH")"
-cd "$BLASTDB" # TODO remove?
+DBDIR="$(dirname "$DBPATH")"
 DBNAME="$(basename "$DBPATH")"
+
+export BLASTDB="$(dirname "$DBDIR")" # TODO DBPATH?
+cd "$DBDIR" # TODO remove?
 
 # TODO query on stdin like the current haskell code?
 # TODO should this .out + .err + mv thing be a regular practice everywhere?
 # TODO and if so, maybe it should be done in haskell? only after it works a couple times the tedious way
-$BLASTCMD $BLASTARGS -db "$DBNAME" -evalue "$EDEC" -outfmt 6 -query "$QPATH" > "${OUTPATH}.out" 2> "${OUTPATH}.err"
+$BLASTCMD -db "$DBNAME" -evalue "$EDEC" -outfmt 6 -query "$QPATH" > "${OUTPATH}.out" 2> "${OUTPATH}.err"
 ln -s "$(basename ${OUTPATH}.out)" "$OUTPATH"
