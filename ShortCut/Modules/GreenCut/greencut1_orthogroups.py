db16d21f747a3765584cc574948081c9456b7f43
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-07-14 15:22:12 -0700

    wip: save start on greencut1 algorithm

--- ShortCut/Modules/GreenCut/greencut1_orthogroups.py
+++ ShortCut/Modules/GreenCut/greencut1_orthogroups.py
@@ -1,5 +1,7 @@
+# This isn't written at all yet...
 
-# simplest algorithm:
+#
+# one algorithm:
 #
 # take pident cutoff, list of rb_hits tables, list of more_hits tables
 #
@@ -10,7 +12,7 @@
 #
 # ...
 
-# Simpler algorithm without loading the chlamy fasta:
+# another variation:
 # take cutoff fraction, list of rb_hits tables, list of more_hits tables
 # for each zipped (rb_hits, more_hits) pair:
 #   make a dict of qid:(cutoff, orthogroup) mappings
@@ -22,3 +24,18 @@
 #     ...
 
 
+# TODO is this only needed for the greencut1 algorithm, not 2?
+def calc_pdists(gen):
+    pdists = {}
+    for hit in gen:
+        if not hit.qid in pdists:
+            pdists[hit.qid] = {}
+        # TODO in rb_hits, there should only ever be one sid right?
+        if not hit.sid in pdists[hit.qid]:
+            pdists[hit.qid][hit.sid] = []
+        assert hit.pident >= 0.0, hit.pident <= 100.0 # TODO rounding errors here?
+        # TODO calculated like they say in the methods, will this exclude all paralogs in chlamy?
+        pdist = 1 - (hit.pident / 100)
+        # print(hit.__dict__)
+        pdists[hit.qid][hit.sid].append(pdist)
+    return pdists
