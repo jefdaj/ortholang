4021fd3dca969e92926efd78ad6caf4d63853818
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-09-12 11:02:15 -0700

    more explicit crb-blast blast+ 2.2.29

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -24,16 +24,16 @@ let
   # ... because it only supports exactly 2.2.29
   # and there are reports of a bug in newer ones (still?)
   # TODO patch crb-blast to use the newest one?
-  ncbi-blast = (pkgs.callPackage ./ncbi-blast {}).overrideDerivation (old: rec {
+  ncbi-blast-2_2_29 = (pkgs.callPackage ./ncbi-blast {}).overrideDerivation (old: rec {
     version="2.2.29";
     name="ncbi-blast-${version}";
     src = pkgs.fetchurl {
-      url = "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/${version}/ncbi-blast-${version}+-x64-linux.tar.gz";
+      url = "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.2.29/ncbi-blast-2.2.29+-x64-linux.tar.gz";
       sha256="1pzy0ylkqlbj40mywz358ia0nq9niwqnmxxzrs1jak22zym9fgpm";
     };
   });
 
-  crb-blast  = pkgs.callPackage ./crb-blast  { inherit ncbi-blast; };
+  crb-blast  = pkgs.callPackage ./crb-blast  { ncbi-blast = ncbi-blast-2_2_29; };
 
   # cdhit = pkgs.callPackage ./cdhit { };
 
