d61472eb1f796ae79ab0cdbbe75887a5f1bdea92
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-09-13 11:53:34 -0700

    integrate timeout into .travis.yml

--- test-travis.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Runs test.sh for up to 30min, then backs up the Nix store.
-# Use to prevent the free version of Travis CI from timing out on big builds.
-# (It will still time out, but you can re-run the job until it finishes)
-
-timeout $((30 * 60)) ./test.sh $@
