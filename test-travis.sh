ae9204ccf0ffe2681ff989a6a2677c89caa15865
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-09-13 14:32:04 -0700

    wip on travis caching

--- test-travis.sh
+++ test-travis.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
-build_cmd="nix-build -j$(nproc) dependencies.nix -A travisBuilds --keep-going"
-storepaths="$($build_cmd --dry-run | grep '\/nix\/store')"
+storepaths="$(nix-build --dry-run | egrep '^\s*\/nix\/store')"
 echo "these store paths will be attempted: $storepaths"
 
 echo "building as many as possible..."
-timeout 600 $build_cmd
+timeout 600 nix-build -j$(nproc) --quiet --keep-going
 
-builtstorepaths="$(for p in $storepaths; do [[ -a $p ]] && echo $p; done)"
-echo "these store paths were built: $builtstorepaths"
-
-echo "caching them for next time..."
+echo "caching the ones that finished for next time..."
 for p in $builtstorepaths; do
+	[[ ! -a $p ]] && continue
+	echo "caching $p"
   nix copy --to file://$HOME/nix.store $p
 done
