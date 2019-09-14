6a3fc5d237e7ed1e1e23586c39b7ba678746ff8f
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-09-14 09:22:12 -0700

    wip: try cachix

--- test.sh
+++ test.sh
@@ -17,11 +17,11 @@ export STACK_ARGS="--allow-different-user"
 export TEST_ARGS="+RTS -IO -N -RTS --test $@"
 
 # this does an incremental build of the haskell code for faster testing
-echo "testing build in nix-shell..."
+echo "testing stack build in nix-shell..."
 cmd="(stack build $STACK_ARGS && stack $STACK_ARGS exec shortcut -- $TEST_ARGS) || exit"
-nix-shell $NIX_ARGS --command "$cmd" 2>&1 | tee test-stack.log
+nix-shell $NIX_ARGS --command "$cmd" 2>&1 | tee test-nix-stack.log
 
 # this builds everything at once, which is simpler.
 # the downside is it rebuilds the haskell code from scratch.
-echo "testing nix-only build (no stack)..."
+echo "testing nix-build only (no stack)..."
 (nix-build $NIX_ARGS && ./result/bin/shortcut $TEST_ARGS) 2>&1 | tee test-nix.log
