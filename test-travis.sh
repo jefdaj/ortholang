580c1ad2a2e77b0166793aefb644ec3a750fba17
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-09-14 09:07:12 -0700

    wip: try cachix

--- test-travis.sh
+++ test-travis.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-storepaths="$(nix-build --dry-run 2>&1 | egrep '^\s*\/nix\/store')"
-echo "these store paths will be attempted:\n$storepaths"
+# storepaths="$(nix-build --dry-run 2>&1 | egrep '^\s*\/nix\/store')"
+# n_storepaths="$(echo "$storepaths" | wc -l)"
+# echo $storepaths
+# echo "trying to build $n_storepaths packages in $n_seconds seconds..."
 
-echo "building as many as possible..."
-timeout 600 nix-build -j$(nproc) --quiet
+n_seconds=60
+timeout $n_seconds nix-build -j$(nproc) --no-build-output 2>&1 | tee build.log
 
-echo "caching the ones that finished for next time..."
-for p in $builtstorepaths; do
-	[[ ! -a $p ]] && continue
-	echo "caching $p"
-  nix copy --to file://$HOME/nix.store $p
+attempted="$(egrep "^building '.*'...$" build.log | cut -d"'" -f2 | sed 's/\.drv//g')"
+for p in $attempted; do
+  nix copy --to file://$HOME/nix.store $p || continue
 done
+# finished="$(for p in $storepaths; do [[ -a $p ]] && echo "$p"; done)"
+# n_finished="$(echo "$finished" | wc -l)"
+# echo "caching the $n_finished packages that finished for next time..."
+# nix copy --to file://$HOME/nix.store $finished
