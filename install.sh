332a00b9f3cdf8a0fbbc39a4b4e7dfc487cb6298
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-09-15 13:32:10 -0700

    draft install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#/usr/bin/env bash
+
+which nix-env &> /dev/null
+if [[ $? != 0 ]]; then
+  echo "Installing the Nix package manager..."
+  curl https://nixos.org/nix/install | sh
+  source $HOME/.nix-profile/etc/profile.d/nix.sh
+fi
+
+echo "Setting Nix to use the ShortCut package cache..."
+nix-env -iA cachix -f https://cachix.org/api/v1/install
+cachix use jefdaj
+
+echo "Installing ShortCut..."
+branch='feature-nostack2nix'
+nix-env -i -f https://github.com/jefdaj/shortcut/archive/${branch}.tar.gz
