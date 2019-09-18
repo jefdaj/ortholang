c47257ec2bf56a278df5e1a721bf457b1c65b50d
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-09-18 00:00:01 +0000

    install from feature-installscript branch

--- install.sh
+++ install.sh
@@ -12,5 +12,5 @@ nix-env -iA cachix -f https://cachix.org/api/v1/install
 cachix use jefdaj
 
 echo "Installing ShortCut..."
-branch='feature-nostack2nix'
+branch='feature-installscript'
 nix-env -i -f https://github.com/jefdaj/shortcut/archive/${branch}.tar.gz
