9c1946c2a9180d66d6edaf1d9d65cd1e17ff3c76
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-02-19 16:27:03 +0000

    split test.sh into nix + stack versions

---
+++ scripts/dev/test-nix.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+set -o pipefail
+
+# only run tests matching this filter string:
+[[ -z "$1" ]] && TEST_FILTER='version' || TEST_FILTER="$1"
+
+if [[ -z "$TMPDIR" ]]; then
+  export TMPDIR=$PWD/.stack-work/tmp
+  mkdir -p "$TMPDIR"
+fi
+
+### build the binary ###
+
+NIX_ARGS="--pure"
+TIMESTAMP=$(date '+%Y-%m-%d_%H:%M')
+LOGFILE="ortholang_${TEST_FILTER}_${TIMESTAMP}.log"
+
+nix-build $NIX_ARGS 2>&1 | tee -a $LOGFILE
+
+export PATH=$PWD/result/bin:$PATH
+
+### run tests ###
+
+# other possible tasty settings: https://hackage.haskell.org/package/tasty
+export TASTY_QUICKCHECK_TESTS=1000
+export TASTY_COLOR="always"
+export TASTY_QUICKCHECK_SHOW_REPLAY=True
+
+# test using shared cache first because it's faster
+ortholang \
+  --shared http://shortcut.pmb.berkeley.edu/shared \
+  --test "$TEST_FILTER" 2>&1 | tee -a $LOGFILE
+
+# then locally to verify everything really works
+ortholang --test "$TEST_FILTER" 2>&1 | tee -a $LOGFILE
---
+++ scripts/dev/test-stack.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+set -o pipefail
+
+# only run tests matching this filter string:
+[[ -z "$1" ]] && TEST_FILTER='version' || TEST_FILTER="$1"
+
+if [[ -z "$TMPDIR" ]]; then
+  export TMPDIR=$PWD/.stack-work/tmp
+  mkdir -p "$TMPDIR"
+fi
+
+### build the binary ###
+
+NIX_ARGS="--pure"
+TIMESTAMP=$(date '+%Y-%m-%d_%H:%M')
+LOGFILE="ortholang_${TEST_FILTER}_${TIMESTAMP}.log"
+
+nix-run() {
+  nix-shell shell.nix $NIX_ARGS --run "$@" 2>&1 | tee -a "$LOGFILE"
+}
+
+### run tests ###
+
+# other possible tasty settings: https://hackage.haskell.org/package/tasty
+export TASTY_QUICKCHECK_TESTS=1000
+export TASTY_COLOR="always"
+export TASTY_QUICKCHECK_SHOW_REPLAY=True
+
+STACK_CMD="stack exec ortholang --"
+TEST_ARGS="--test '$TEST_FILTER'"
+
+# test using shared cache first because it's faster
+nix-run "$STACK_CMD --shared http://shortcut.pmb.berkeley.edu/shared $TEST_ARGS"
+
+# then locally to verify everything really works
+nix-run "$STACK_CMD $TEST_ARGS"
