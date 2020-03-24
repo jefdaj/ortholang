e603b1a6542074575f2b684f6ba8ee9c0c89b439
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-03-24 17:06:29 +0000

    start writing newrulestest1 fn

--- OrthoLang/Modules/NewRulesTest.hs
+++ OrthoLang/Modules/NewRulesTest.hs
@@ -1,13 +1,26 @@
 module OrthoLang.Modules.NewRulesTest where
 
 import OrthoLang.Core.Types
+import OrthoLang.Core.Compile.Basic (defaultTypeCheck)
+import OrthoLang.Modules.SeqIO (faa)
+
 
 orthoLangModule :: OrthoLangModule
 orthoLangModule = OrthoLangModule
   { mName = "NewRulesTest"
   , mDesc = "Test module for the 'new rules' infrastructure"
-  , mTypes = []
+  , mTypes = [str, faa]
   , mFunctions =
-      [ 
+      [ newRulesTest1
       ]
+  }
+
+newRulesTest1 :: OrthoLangFunction
+newRulesTest1 = let name = "newrulestest1" in OrthoLangFunction
+  { fNames     = [name]
+  , fTypeDesc  = mkTypeDesc  name [str, faa] str
+  , fTypeCheck = defaultTypeCheck [str, faa] str
+  , fFixity    = Prefix, fTags = []
+  , fOldRules = undefined
+  , fNewRules = Just (return ())
   }
---
+++ tests/scripts/newrulestest.ol
@@ -0,0 +1,4 @@
+f = load_faa "examples/sequences/Mycoplasma_mycoides_protein_refseq.faa"
+s = "this is a string"
+test1 = newrulestest1 s f
+result = test1
