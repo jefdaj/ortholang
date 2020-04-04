b422111ad7e65a2863da5b36c6aa6601d730a823
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-04-04 13:35:35 +0000

    add summarize-tests.sh

---
+++ scripts/dev/summarize-tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+LOG=test.log
+
+echo "passed: $(grep OK $LOG | wc -l)"
+echo "failed: $(grep FAIL $LOG | wc -l)"
+echo "timeout: $(grep TIMEOUT $LOG | wc -l)"
+
+echo "tmpfiles changed: $(git status | grep tests | grep tmpfiles | wc -l)"
+echo "stdouts changed: $(git status | grep tests | grep stdout | wc -l)"
