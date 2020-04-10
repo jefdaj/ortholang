c8b35e6ea63ffdd87c8567665101bac068b80437
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-04-10 18:25:33 +0000

    add macro expansions to summarzietests [ci:skip]

--- scripts/dev/summarize-tests.sh
+++ scripts/dev/summarize-tests.sh
@@ -8,3 +8,4 @@ echo "timeout: $(grep TIMEOUT $LOG | wc -l)"
 
 echo "tmpfiles changed: $(git status | grep tests | grep tmpfiles | wc -l)"
 echo "stdouts changed: $(git status | grep tests | grep stdout | wc -l)"
+echo "expands changed: $(git status | grep tests | grep expand | wc -l)"
