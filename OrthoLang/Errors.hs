1c35e0dfdfe5c9d428f938d5b2e24cf4fb708a75
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-04-11 16:20:00 +0000

    wip: Errors module [ci:skip]

--- OrthoLang/Errors.hs
+++ OrthoLang/Errors.hs
@@ -11,16 +11,21 @@ possible. Then you can 'Control.Exception.catch' them using any superclass up
 through 'Control.Exception.SomeException':
 
 @
+λ: throw (Mistake ["this is a hint", "another hint"]) \`catch\` \\e -> putStrLn $ show (e :: SomeOLException)
+Programming mistake! Please report it to Jeff Johnson:
+this is a hint
+another hint
+
 λ: throw (NoSuchVar "bob") \`catch\` \\e -> putStrLn $ show (e :: SomeParserException)
 no such var: 'bob'
 
 λ: throw (ScriptFailed "test.sh" "test.log" 3) \`catch\` \\e -> putStrLn (show (e :: SomeEvalException))
 test.sh failed with exit code 3. Check test.log for details.
 
-λ: throw (pleaseReport ["this is a hint", "another hint"]) \`catch\` \\e -> putStrLn $ show (e :: SomeOLException)
-Programming error! Please report it to Jeff Johnson:
-this is a hint
-another hint
+λ: throw (MissingDigest "aTestAction" "$TMPDIR\/exprs\/whatever\/result" "230478234")
+  \`catch\` \\e -> putStrLn $ show (e :: SomeCompilerException)
+Missing digest '230478234', which is needed by aTestAction to build '$TMPDIR/exprs/whatever/result'
+
 @
 
 If none of the superclasses match, you get an actual error:
@@ -38,13 +43,14 @@ module OrthoLang.Errors
   (
 
   -- * Generic exceptions
-    pleaseReport
+    Mistake(..)
 
   -- * Parser exceptions
   , NoSuchVar(..)
   -- TODO type errors (a hierarchy?)
 
   -- * Compiler exceptions
+  , MissingDigest(..)
   -- TODO type errors (a hierarchy?)
 
   -- * Repl exceptions
@@ -69,9 +75,9 @@ module OrthoLang.Errors
 import Control.Exception (Exception, SomeException, fromException, toException, ErrorCall(..))
 import Data.Typeable     (Typeable, cast)
 
-----------------
--- base class --
-----------------
+------------------------
+-- generic exceptions --
+------------------------
 
 data SomeOLException = forall e . Exception e => SomeOLException e
   deriving Typeable
@@ -89,9 +95,14 @@ olFromException x = do
   SomeOLException a <- fromException x
   cast a
 
-pleaseReport :: [String] -> SomeException
-pleaseReport hints = olToException $ ErrorCall $ unlines $
-  "Programming error! Please report it to Jeff Johnson:" : hints
+data Mistake = Mistake [String]
+
+instance Show Mistake where
+  show (Mistake hints) = unlines $ "Programming mistake! Please report it to Jeff Johnson:" : hints
+
+instance Exception Mistake where
+  toException   = olToException
+  fromException = olFromException
 
 -----------------------
 -- parser exceptions --
@@ -117,7 +128,7 @@ parserFromException x = do
 data NoSuchVar = NoSuchVar String
 
 instance Show NoSuchVar where
-  show (NoSuchVar var) = "no such var: '" ++ var ++ "'"
+  show (NoSuchVar var) = "No such var: '" ++ var ++ "'"
 
 instance Exception NoSuchVar where
   toException   = parserToException
@@ -143,6 +154,17 @@ compilerFromException :: Exception e => SomeException -> Maybe e
 compilerFromException x = do
   SomeCompilerException a <- fromException x
   cast a
+
+-- TODO Path/PathDigest instead of FilePath/String? if it works without an import cycle
+data MissingDigest = MissingDigest String FilePath String
+
+instance Show MissingDigest where
+  show (MissingDigest act path dig) =
+    "Missing digest '" ++ dig ++ "', which is needed by " ++ act ++ " to build '" ++ path ++ "'"
+
+instance Exception MissingDigest where
+  toException   = compilerToException
+  fromException = compilerFromException
 
 ---------------------
 -- repl exceptions --
