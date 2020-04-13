ecf8db7ddc4560d64692591ca2f91f7d4e7c1450
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-04-13 17:12:42 +0000

    wip: start debugging muscle.sh [ci:skip]

--- OrthoLang/Modules/Muscle/muscle.sh
+++ OrthoLang/Modules/Muscle/muscle.sh
@@ -2,8 +2,15 @@
 
 set -e
 
-OUTPATH="$1"
+RESPATH="$1"
+OUTPATH="$(dirname "$RESPATH")/out"
+ERRPATH="$(dirname "$RESPATH")/err"
 FAPATH="$2"
 
 # TODO temporary outpath then mv/ln?
-muscle -clwstrict -in "$FAPATH" -out "${OUTPATH}" > "${OUTPATH}.out" 2> "${OUTPATH}.err"
+muscle -clwstrict -in "$FAPATH" -out "${RESPATH}" > "$OUTPATH" 2> "$ERRPATH"
+
+# code="$?"
+# cp "$OUTPATH" /tmp/muscle-out.txt
+# cp "$ERRPATH" /tmp/muscle-err.txt
+# exit $code
