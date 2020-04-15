fd04baa96beb97c24c6ec0dd243423e819c21f80
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-04-15 18:32:49 +0000

    tweak summarize-tests.sh [ci:skip]

--- scripts/dev/summarize-tests.sh
+++ scripts/dev/summarize-tests.sh
@@ -2,10 +2,12 @@
 
 LOG=test.log
 
-echo "passed: $(grep OK $LOG | wc -l)"
-echo "failed: $(grep FAIL $LOG | wc -l)"
-echo "timeout: $(grep TIMEOUT $LOG | wc -l)"
-
+echo "tests passed: $(grep OK $LOG | wc -l)"
+echo
+echo "tests timed out: $(grep TIMEOUT $LOG | wc -l)"
+echo "important tests failed: $(grep FAIL $LOG | grep -v help | wc -l)"
+echo "trivial tests failed: $(grep FAIL $LOG | grep    help | wc -l)"
+echo
 echo "tmpfiles changed: $(git status | grep tests | grep tmpfiles | wc -l)"
 echo "stdouts changed: $(git status | grep tests | grep stdout | wc -l)"
 echo "expands changed: $(git status | grep tests | grep expand | wc -l)"
