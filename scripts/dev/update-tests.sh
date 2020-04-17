bec28ef8e7d97141b406de0a0ed5f5acb7c89990
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-04-17 16:47:12 +0000

    merge into develop and update tests

---
+++ scripts/dev/update-tests.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Usage:
+# 1) nix-shell
+# 2) ./scripts/dev/update-test.sh
+# 3) while that runs, periodically:
+#      git diff tests
+#      check that the changes make sense
+#      git add tests
+
+set -e
+
+stack build
+
+logfile="test.log"
+testdir="tests"
+stacktestdir="$(find .stack-work -type d -name tests)"
+
+# delete golden files
+for d in ${stacktestdir}/*; do
+  [[ $d =~ 'scripts' ]] || rm -rf  $d/*
+done
+
+update_tests() {
+  while sleep 10; do
+    rsync -r ${stacktestdir}/* ${testdir}/
+  done
+}
+
+# run tests to remake them, while syncing back to main tests dir
+update_tests &
+pid=$?
+stack exec ortholang -- --test all 2>&1 | tee "$logfile"
+
+# wait for one last rsync, then kill it
+sleep 15
+kill -9 $pid || true
