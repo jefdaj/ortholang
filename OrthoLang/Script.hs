2297561f97599f1041749194208622c1750173bf
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-04-25 16:21:00 +0000

    organize Script module a bit [ci:skip]

--- OrthoLang/Script.hs
+++ OrthoLang/Script.hs
@@ -1,102 +1,52 @@
 {-|
-TODO move all script transformations here if possible and document them nicely
+This module gathers together all the semi-complicated 'Script' transformations
+that were littering the rest of the codebase.
+
+TODO use the standard module hierarchy? Data.OrthoLang.Script sounds good
 -}
 
 module OrthoLang.Script
   (
 
-    rDepsOf
+  -- * Used in Interpreter.Parse
+    assign
+
+  -- * Used in Modules.Replace
   , extractExprs
-  , updateVars
-  , replaceVar
-  , removeSelfReferences
-  , rmRef
-  , assign
   , setRepIDs
+  -- , mapExprVars
+  -- , mapAssignVars
+  -- , mapScriptVars
+  -- , setRepID
+
+  -- * Used in Interpreter.Repl
+  , rDepsOf
   , depsOnly
+  , updateVars
+  -- , replaceVar
+  -- , removeSelfReferences
+  -- , rmRef
 
   )
   where
 
 import Prelude hiding (error)
 import OrthoLang.Debug (trace, error)
+
 import OrthoLang.Types
+import Data.List       (filter, delete)
+import OrthoLang.Util  (justOrDie)
 
-import Data.List           (filter, delete)
-import OrthoLang.Util               (absolutize, justOrDie)
 
-rDepsOf :: Script -> Var -> [Var]
-rDepsOf s var = map aVar rDeps
-  where
-    rDeps = filter (isRDep . aExpr) (sAssigns s)
-    isRDep expr = elem var $ depsOf expr
-
--- TODO what if it's a function call?
--- do we have to make a rule that you can't use those?
--- (uuuugly! but not a show-stopper for now)
-extractExprs :: Script -> Expr -> [Expr]
-extractExprs  _  (Lst _ _ es) = es
-extractExprs s (Ref _ _ _ v ) = case lookupVar v (sAssigns s) of
-                                       Nothing -> error "types.extractExprs" $ "no such var " ++ show v
-                                       Just e  -> extractExprs s e
-extractExprs _   (Fun _ _ _ _ _) = error "types.extractExprs" explainFnBug
-extractExprs scr (Bop _ _ _ _ l r) = extractExprs scr l ++ extractExprs scr r
-extractExprs  _   e               = error "types.extractExprs" $ "bad arg: " ++ show e
-
--- TODO will this get printed, or will there just be a parse error?
-explainFnBug :: String
-explainFnBug =
-  "You've stumbled on an outstanding bug. Sorry about that! \
-  \The problem is that when doing transformations involving lists \
-  \like repeat or map, OrthoLang can't \"see\" through future function calls; \
-  \it can only manipulate lists whose elements are known *before* running the \
-  \program. If you want Jeff to consider rewriting some things to fix that, \
-  \drop him a line!"
-
--- this is needed to avoid assigning a variable literally to itself,
--- which is especially a problem when auto-assigning "result"
--- TODO is this where we can easily require the replacement var's type to match if it has deps?
--- TODO what happens if you try that in a script? it should fail i guess?
--- TODO rename because it's more like assignAndUpdateVars?
-updateVars :: Script -> Assign -> Script
-updateVars scr asn@(Assign {aVar = v@(Var _ vName), aExpr = expr}) = scr {sAssigns = as', sResult = r'}
-  where
-    res = Var (RepID Nothing) "result"
-    asn' = removeSelfReferences scr asn
-    as  = sAssigns scr
-    as' = if v /= res && aVar asn `elem` map aVar as
-            then replaceVar asn' as
-            else delVar as vName ++ [asn']
-    r' = case sResult scr of
-           Nothing -> Just expr -- no previous result, so assign it now
-           Just re -> if v == res then Just expr else sResult scr
-
--- replace an existing var in a script
-replaceVar :: Assign -> [Assign] -> [Assign]
-replaceVar a1 = map $ \a2 -> if aVar a1 == aVar a2 then a1 else a2
-
--- makes it ok to assign a var to itself in the repl
--- by replacing the reference with its value at that point
--- TODO forbid this in scripts though
-removeSelfReferences :: Script -> Assign -> Assign
-removeSelfReferences s a@(Assign {aVar=v, aExpr=e}) = if not (v `elem` depsOf e) then a else a {aExpr=rmRef s v e}
-
--- does the actual work of removing self-references
-rmRef :: Script -> Var -> Expr -> Expr
-rmRef scr var e@(Ref _ _ _ v2)
-  | var == v2 = justOrDie "failed to rmRef variable!" $ lookupVar var (sAssigns scr)
-  | otherwise = e
-rmRef _   _   e@(Lit _ _) = e
-rmRef scr var (Bop  t ms vs s e1 e2) = Bop t ms (delete var vs) s (rmRef scr var e1) (rmRef scr var e2)
-rmRef scr var (Fun  t ms vs s es   ) = Fun t ms (delete var vs) s (map (rmRef scr var) es)
-rmRef scr var (Lst t vs       es   ) = Lst t    (delete var vs)   (map (rmRef scr var) es)
-rmRef _   _   (Com _) = error "types.rmRef" "implement this! or rethink?"
+-----------
+-- parse --
+-----------
 
 -- Ref Type (Maybe Salt) [Var] Var -- do refs need a salt? yes! (i think?)
 -- TODO salt is Nothing rather than the expr's salt, right?
 -- TODO var is added to deps, right?
 assign :: Script -> Assign -> Script
-assign scr a@(Assign var@(Var _ vName) expr) =
+assign scr a =
 
   -- let rv  = Var (RepID Nothing) "result"
       -- rr  = Ref (typeOf expr) Nothing (depsOf expr) var 
@@ -163,17 +113,37 @@ assign scr a@(Assign var@(Var _ vName) expr) =
            ("old scr:\n" ++ render (pPrint scr) ++ "\nnew scr'':\n" ++ render (pPrint scr''))
            scr''
 
----------------------------
--- mangle variable names --
----------------------------
+
+-------------
+-- replace --
+-------------
+
+-- TODO what if it's a function call?
+-- do we have to make a rule that you can't use those?
+-- (uuuugly! but not a show-stopper for now)
+extractExprs :: Script -> Expr -> [Expr]
+extractExprs  _  (Lst _ _ es) = es
+extractExprs s (Ref _ _ _ v ) = case lookupVar v (sAssigns s) of
+                                       Nothing -> error "types.extractExprs" $ "no such var " ++ show v
+                                       Just e  -> extractExprs s e
+extractExprs _   (Fun _ _ _ _ _) = error "types.extractExprs" explainFnBug
+extractExprs scr (Bop _ _ _ _ l r) = extractExprs scr l ++ extractExprs scr r
+extractExprs  _   e               = error "types.extractExprs" $ "bad arg: " ++ show e
+
+-- TODO will this get printed, or will there just be a parse error?
+explainFnBug :: String
+explainFnBug =
+  "You've stumbled on an outstanding bug. Sorry about that! \
+  \The problem is that when doing transformations involving lists \
+  \like repeat or map, OrthoLang can't \"see\" through future function calls; \
+  \it can only manipulate lists whose elements are known *before* running the \
+  \program. If you want Jeff to consider rewriting some things to fix that, \
+  \drop him a line!"
 
 {- This does the filename mangling by setting a "replace ID" in each variable
  - in a script. If it's anything other than Nothing it gets used by
  - OrthoLang.Interpreter to set the rep dir.
- -
- - TODO should it go in Types, or maybe Paths?
  -}
-
 mapExprVars :: (Var -> Var) -> Expr -> Expr
 mapExprVars _ e@(Lit  _ _) = e
 mapExprVars fn (Ref  t n vs v      ) = Ref  t n (map fn vs)   (fn v)
@@ -195,8 +165,57 @@ setRepIDs :: RepID -> Script -> Script
 setRepIDs newID = mapScriptVars (setRepID newID)
 
 
+----------
+-- repl --
+----------
+
+rDepsOf :: Script -> Var -> [Var]
+rDepsOf s var = map aVar rDeps
+  where
+    rDeps = filter (isRDep . aExpr) (sAssigns s)
+    isRDep expr = elem var $ depsOf expr
+
 depsOnly :: Expr -> Script -> Script
 depsOnly expr scr = scr {sAssigns = deps ++ [res]}
   where
     deps = filter (\a -> (elem (aVar a) $ depsOf expr)) (sAssigns scr)
     res  = Assign {aVar = Var (RepID Nothing) "result", aExpr = expr}
+
+-- this is needed to avoid assigning a variable literally to itself,
+-- which is especially a problem when auto-assigning "result"
+-- TODO is this where we can easily require the replacement var's type to match if it has deps?
+-- TODO what happens if you try that in a script? it should fail i guess?
+-- TODO rename because it's more like assignAndUpdateVars?
+updateVars :: Script -> Assign -> Script
+updateVars scr asn@(Assign {aVar = v@(Var _ vName), aExpr = expr}) = scr {sAssigns = as', sResult = r'}
+  where
+    res = Var (RepID Nothing) "result"
+    asn' = removeSelfReferences scr asn
+    as  = sAssigns scr
+    as' = if v /= res && aVar asn `elem` map aVar as
+            then replaceVar asn' as
+            else delVar as vName ++ [asn']
+    r' = case sResult scr of
+           Nothing -> Just expr -- no previous result, so assign it now
+           Just _  -> if v == res then Just expr else sResult scr
+
+-- replace an existing var in a script
+replaceVar :: Assign -> [Assign] -> [Assign]
+replaceVar a1 = map $ \a2 -> if aVar a1 == aVar a2 then a1 else a2
+
+-- makes it ok to assign a var to itself in the repl
+-- by replacing the reference with its value at that point
+-- TODO forbid this in scripts though
+removeSelfReferences :: Script -> Assign -> Assign
+removeSelfReferences s a@(Assign {aVar=v, aExpr=e}) = if not (v `elem` depsOf e) then a else a {aExpr=rmRef s v e}
+
+-- does the actual work of removing self-references
+rmRef :: Script -> Var -> Expr -> Expr
+rmRef scr var e@(Ref _ _ _ v2)
+  | var == v2 = justOrDie "failed to rmRef variable!" $ lookupVar var (sAssigns scr)
+  | otherwise = e
+rmRef _   _   e@(Lit _ _) = e
+rmRef scr var (Bop  t ms vs s e1 e2) = Bop t ms (delete var vs) s (rmRef scr var e1) (rmRef scr var e2)
+rmRef scr var (Fun  t ms vs s es   ) = Fun t ms (delete var vs) s (map (rmRef scr var) es)
+rmRef scr var (Lst t vs       es   ) = Lst t    (delete var vs)   (map (rmRef scr var) es)
+rmRef _   _   (Com _) = error "types.rmRef" "implement this! or rethink?"
