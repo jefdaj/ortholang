2ff5da2c449786abc03c1ab3753f4f29b617f7d3
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-05-18 14:02:02 -0700

    bugfix: check scripts strip decimals

--- tests/check/blasthits:best_hits.sh
+++ tests/check/blasthits:best_hits.sh
@@ -1,4 +1,4 @@
 # The number of hits varies pretty wildly, but there should at least be a few!
-output="$(cat "${1}/output.txt")"
+output="$(cat "${1}/output.txt" | cut -d'.' -f1)"
 minhits=10
 [[ $output -ge $minhits ]] || (echo -n "less than $minhits hits: $output" && exit 1)
