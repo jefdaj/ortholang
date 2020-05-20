6162163dd570c815395b42473917bb9dd3e73cc1
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-05-19 21:07:00 -0700

    wip: new idea

--- OrthoLang/Interpreter/Compile/NewMap.hs
+++ OrthoLang/Interpreter/Compile/NewMap.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-
 {-|
-
+The simplest way I can think to implement mapping is at the Action level, using
+something similar to the macros above. The idea is that Shake can figure out
+how to produce the mapped function elements, as long as we provide a list of
+the paths we need.
 -}
 
 module OrthoLang.Interpreter.Compile.NewMap
@@ -16,9 +17,7 @@ module OrthoLang.Interpreter.Compile.NewMap
   , newMap3of3
 
   -- * Implementation
-  -- , NewMapTemplate
-  -- , newMapTemplate
-  , newMapRules
+  , newMap1
 
   )
   where
@@ -34,101 +33,50 @@ import OrthoLang.Interpreter.Compile.NewRules
 -- interface --
 ---------------
 
--- type NewAction1 = ExprPath -> FilePath                         -> Action ()
--- type NewAction2 = ExprPath -> FilePath -> FilePath             -> Action ()
--- type NewAction3 = ExprPath -> FilePath -> FilePath -> FilePath -> Action ()
+newMap1of1 :: String -> NewAction1 -> NewAction1
+newMap1of1 prefix act o@(ExprPath o') l1' = undefined
+  -- let loc = "ortholang.interpreter.compile.newmap.newMap1of1"
+  -- template <- calcTemplate o 1
+  -- elems <- readList loc l1' -- TODO find out the type and use readstrings?
+  -- undefined
 
-type NewMap1 = ExprPath -> FilePath                         -> NewAction1
-type NewMap2 = ExprPath -> FilePath -> FilePath             -> NewAction2
-type NewMap3 = ExprPath -> FilePath -> FilePath -> FilePath -> NewAction3
+newMap1of2 :: String -> NewAction2 -> NewAction2
+newMap1of2 prefix act (ExprPath o) l1 p2 = undefined
 
-newMap1of1 :: NewMap1
-newMap1of1 (ExprPath o) l1 = undefined
+newMap2of2 :: String -> NewAction2 -> NewAction2
+newMap2of2 prefix act (ExprPath o) p1 l2 = undefined
 
-newMap1of2 :: NewMap2
-newMap1of2 (ExprPath o) l1 p2 = undefined
+newMap1of3 :: String -> NewAction3 -> NewAction3
+newMap1of3 prefix act (ExprPath o) l1 p2 p3 = undefined
 
-newMap2of2 :: NewMap2
-newMap2of2 (ExprPath o) p1 l2 = undefined
+newMap2of3 :: String -> NewAction3 -> NewAction3
+newMap2of3 prefix act (ExprPath o) p1 l2 p3 = undefined
 
-newMap1of3 :: NewMap3
-newMap1of3 (ExprPath o) l1 p2 p3 = undefined
-
-newMap2of3 :: NewMap3
-newMap2of3 (ExprPath o) p1 l2 p3 = undefined
-
-newMap3of3 :: NewMap3
-newMap3of3 (ExprPath o) p1 p2 l3 = undefined
+newMap3of3 :: String -> NewAction3 -> NewAction3
+newMap3of3 prefix act (ExprPath o) p1 p2 l3 = undefined
 
 
 --------------------
 -- implementation --
 --------------------
 
--- data MappedExpr = MappedExpr Type ExprPath (Rules ExprPath)
+-- this is everything we need to make a standard expr path:
+--
+-- exprPathExplicit :: Config -> String -> Maybe Seed -> [String] -> Path
+-- exprPathExplicit cfg prefix mSeed hashes = toPath loc cfg path
+--
+-- could that be constructed based on nothing but the final outpath and a prefix?
 
-{-|
-String that will be readable into a Fun Expr once  is
-replaced with a suitable path. The idea is we can save it to a file, then read
-back that file and fill in the template to create each of the mapped Exprs.
+-- we start with the single-list-only case, and will generalize if it works
+newMap1 :: String -> NewAction1 -> NewAction1
+newMap1 prefix actFn (ExprPath outPath) arg1Path = do
+  -- TODO get cfg
+  -- TODO replace prefix in outPath
+  -- TODO digest arg1Path, then replace that digest in outPath with each of its elements?
+  --      no wait, less magical to use the index: hash 1 for this one
+  -- TODO return the list and we're done?
+  undefined
 
-TODO does it always need to be a Fun?
--}
-newtype NewMapTemplate = NewMapTemplate String
-  deriving (Read, Show, Eq, Ord)
-
-{-
-Create a NewMapTemplate by getting the exprPath of the Expr, then replacing
-one of the arg hashes. Note that the Expr is expected to have an invalid arg
-type, so it is not possible to compile except with the newmap machinery.
-
-The inserted MapHere will have a MapID derived from the Expr it replaces. That
-eliminates confusion about which Exprs to replace with which elements later.
-
-The thing to be mapped over should always be a list, so we set the map
-placeholder type to the type of each list element.
-
-TODO write another version of exprPath without the requirement for dRef?
--}
--- newMapTemplate :: Config -> DigestsRef -> Script -> Int -> Expr -> NewMapTemplate
--- newMapTemplate _ _ _ i _ | i < 0 = error "ortholang.interpreter.compile.newmap.newMapTemplate"
---                                           $ "bad arg: " ++ show i
--- 
--- newMapTemplate cfg dRef scr i (Fun r ms ds n es) | length es > i = NewMapTemplate $ show fn'
---   where
---     exprToReplace = es !! i
---     (ListOf eType) = typeOf exprToReplace
---     mapid = MapID $ digest $ exprPath cfg dRef scr exprToReplace
---     placeholder = Map eType (seedOf exprToReplace)
---                             (depsOf exprToReplace)
---                             (MapHere mapid)
---     es' = replace es (i, placeholder)
---     fn' = Fun r ms ds n es'
--- 
--- newMapTemplate _ _ _ _ e = error "ortholang.interpreter.compile.newmap.newMapTemplate"
---                                   $ "bad arg: " ++ show e
-
--- replace the Nth element in a list
-replace :: (Num a, Ord a) => [b] -> (a,b) -> [b]
-replace [] _ = []
-replace (_:xs) (0,a) = a:xs
-replace (x:xs) (n,a) =
-  if n < 0
-    then (x:xs)
-    else x: replace xs (n-1,a)
-
-{-|
-The "new map" machinery works like so:
-
-1. Save a  "template" file, which holds a shown Expr with a MapHere inside
-2. Save an "elements" file, which is a list of paths that should be substituted in
-3. Read those two to create a list of new Exprs
-4. Compile the new Exprs normally (via rExpr)
-
-TODO can the elements file be removed? Should be able to tell by the fn call path
-
-TODO wait why was this necessary at all? can you just extractExprs by needing it first?
--}
-newMapRules :: Rules ()
-newMapRules = do
-  return ()
+-- calcTemplate :: ExprPath -> Int -> Action ExprPathTemplate
+-- calcTemplate path index = do
+  -- undefined
