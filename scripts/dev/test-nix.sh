b53d166743318c2ab986079a1cc67c9b169abbe2
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-05-30 14:02:57 -0700

    update test scripts

--- scripts/dev/test-nix.sh
+++ scripts/dev/test-nix.sh
@@ -4,15 +4,15 @@ set -x
 set -o pipefail
 
 # start with the basic tests
-TEST_FILTER='$2 ~/version/ || $2 ~/repl/ || $2 ~/parser/ || $5 ~/parses/ || $5 ~/expands/'
+testfilter='$2 ~/version/ || $2 ~/repl/ || $2 ~/parser/ || $5 ~/parses/ || $5 ~/expands/'
 
 # add module-specific scripts if any
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 [[ "$BRANCH" =~ module ]] \
-  && TEST_FILTER="$TEST_FILTER || \$5 ~/$(echo "$BRANCH" | cut -d'-' -f2):/"
+  && testfilter="$testfilter || \$5 ~/$(echo "$BRANCH" | cut -d'-' -f2):/"
 
 # override from the command line
-[[ -z "$1" ]] || TEST_FILTER="$1"
+[[ -z "$1" ]] || testfilter="$1"
 
 if [[ -z "$TMPDIR" ]]; then
   export TMPDIR=$PWD/.stack-work/tmp
@@ -23,7 +23,7 @@ fi
 
 NIX_ARGS="" # TODO put back --pure?
 # TIMESTAMP=$(date '+%Y-%m-%d_%H:%M')
-# LOGFILE="ortholang_${TEST_FILTER}_${TIMESTAMP}.log"
+# LOGFILE="ortholang_${testfilter}_${TIMESTAMP}.log"
 LOGFILE='test.log'
 
 nix-build $NIX_ARGS 2>&1 | tee -a $LOGFILE
@@ -45,7 +45,7 @@ export TASTY_QUICKCHECK_TESTS=1000
 export TASTY_COLOR="always"
 export TASTY_QUICKCHECK_SHOW_REPLAY=True
 
-# TEST_ARGS=bin-run "debug '.*' --test $TEST_FILTER"
+# TEST_ARGS=bin-run "debug '.*' --test $testfilter"
 # test using shared cache first because it's faster
 # TODO put back once server is back up
 # bin-run --shared http://shortcut.pmb.berkeley.edu/shared $TEST_ARGS
@@ -58,4 +58,4 @@ export TASTY_QUICKCHECK_SHOW_REPLAY=True
 # exit $code2
 
 # local tests only pending server update
-./result/bin/ortholang --debug '.*' --test "$TEST_FILTER" 2>&1 | tee -a $LOGFILE
+./result/bin/ortholang --debug '.*' --test "$testfilter" 2>&1 | tee -a $LOGFILE
