37bfec081ae6deaf81b95a8c2ced171be4fe6155
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-06-02 16:43:15 -0700

    wip: rewrite reciprocal_best_all

--- OrthoLang/Modules/BlastRBH.hs
+++ OrthoLang/Modules/BlastRBH.hs
@@ -146,6 +146,29 @@ reciprocalBest = newFnS2
 
 -- TODO rewrite with newFnS2
 
+-- reciprocalBestAll :: Function
+-- reciprocalBestAll = Function
+--   { fOpChar = Nothing, fName = name
+--   , fInputs = [Exactly (ListOf bht), Exactly (ListOf bht)] -- TODO any ht would work right?
+--   , fOutput = Exactly bht
+--   ,fTags = []
+--   , fNewRules = NewNotImplemented, fOldRules = rSimple aReciprocalBestAll
+--   }
+--   where
+--     name = "reciprocal_best_all"
+
+-- aReciprocalBestAll :: [Path] -> Action ()
+-- aReciprocalBestAll (out:ins) = do
+--   cfg <- fmap fromJust getShakeExtra
+--   let cDir = fromPath loc cfg $ cacheDir cfg "blastrbh"
+--       loc = "modules.blastrbh.aReciprocalBestAll"
+--       tmpPath p = cDir </> digest loc p <.> "bht"
+--       ins' = map (\p -> (p, tmpPath p)) $ map (fromPath loc cfg) ins
+--   liftIO $ createDirectoryIfMissing True cDir
+--   mapM_ (\(inPath, outPath) -> absolutizePaths loc inPath outPath) ins'
+--   aSimpleScriptNoFix "reciprocal_best_all.R" (out:map (toPath loc cfg . snd) ins')
+-- aReciprocalBestAll ps = error $ "bad argument to aReciprocalBestAll: " ++ show ps
+
 reciprocalBestAll :: Function
 reciprocalBestAll = newFnS2
   "reciprocal_best_all"
