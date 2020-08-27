7f370c514f191b1c244043e16993d718041f62b3
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-08-26 17:04:07 -0700

    wip: clean up CustomScript comments

--- OrthoLang/Modules/CustomScript.hs
+++ OrthoLang/Modules/CustomScript.hs
@@ -1,42 +1,9 @@
-{- This module lets you "cheat" the type system by calling your own script and
- - specifying the return type as a string. Why would you want that, when the
- - whole idea of OrthoLang is that it makes sure scripts fit together smoothly?
- - I can think of two situations in which it might be helpful:
- -
- - 1. You need to extend OrthoLang, but can't figure out Haskell or don't want
- -    to bother writing a module.
- -
- - 2. You are developing a OrthoLang module, but want to make sure the "useful"
- -    parts work before fiddling with the wrapper code. (This is me sometimes)
- -
- - More generally, I think it's important never to lock people into using a
- - specific set of abstractions. Especially in science! Odds are your research
- - includes complicated issues I can't forsee, and you might need an "escape
- - hatch" to get around my clever rules.
- -
- - Anyway, this provides one function called "cheat". It takes two strings: the
- - script itself (relative path, absolute path, or basename of something on
- - your PATH) and the name of the OrthoLang type to assign to the result.
- -
- - You can use a nonexistent OrthoLang type here if you want. The only problem
- - is it won't be usable by any other (non-cheat) functions afterward.
- -
- - After those two strings, any number of other arguments are allowed. Their
- - filenames will be passed to the script along with the standard return path
- - and temporary directory.
- -}
-
--- TODO write examples
--- TODO write cheatTypeCheck
--- TODO write rCustomScript
--- TODO come up with more neutral word than cheat?
--- TODO have a help menu that lists current types? not until after meeting!
-
 module OrthoLang.Modules.CustomScript where
 
 import Development.Shake
 import OrthoLang.Types
 import OrthoLang.Interpreter
+
 import OrthoLang.Debug (error)
 import Prelude hiding (error)
 import Data.Maybe (fromJust)
