042a1d775ea896d77d2f4c1702307fabcc19713b
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-09-03 09:13:08 -0700

    abort cached date range stuff and go with day only

--- time-test.hs
+++ time-test.hs
@@ -1,5 +1,11 @@
 module Main where
 
+-- TODO looking good! next step is to add parsing
+-- minimum parsing that would work:
+--   always require an exact YYYY-MM-DD, but ignore seconds
+--   if it's in the future/past/not available/invalid, ask to switch to today and re-run
+--   conflicting files: overwrite the same day, assuming it's fixing an error, and re-run everything
+
 import Data.Time
 import Text.Printf
 import System.FilePath ((</>))
@@ -7,30 +13,30 @@ import Data.List.Split (splitOn)
 
 nowDatedDir :: IO FilePath
 nowDatedDir = do
-  (UTCTime date secs) <- getCurrentTime
+  (UTCTime date _) <- getCurrentTime
   let (year, month, day) = toGregorian date
       sYear  = printf "%04d" year
       sMonth = printf "%02d" month
       sDay   = printf "%02d" day
-      sSecs  = show (round secs :: Integer)
-      dir    = sYear </> sMonth </> sDay </> sSecs
+      -- sSecs  = show (round secs :: Integer)
+      dir    = sYear </> sMonth </> sDay -- </> sSecs
   return dir
 
 -- TODO return a Maybe?
 dirToDay :: FilePath -> Day
 dirToDay dir = fromGregorian nYear nMonth nDay
   where
-    (_:day:month:year:_) = reverse $ splitOn "/" dir
+    (day:month:year:_) = reverse $ splitOn "/" dir
     nYear  = read year  :: Integer
     nMonth = read month :: Int
     nDay   = read day   :: Int
 
-isCachedDataValid :: Day -> IO Bool
-isCachedDataValid dayFetched = do
-  (UTCTime currentDay _) <- getCurrentTime
-  let lastDayValid = addDays 90 dayFetched -- TODO configure the validity time?
-      daysLeft = diffDays lastDayValid currentDay
-  return $ daysLeft > 0
+-- isCachedDataValid :: Day -> IO Bool
+-- isCachedDataValid dayFetched = do
+--   (UTCTime currentDay _) <- getCurrentTime
+--   let lastDayValid = addDays 90 dayFetched -- TODO configure the validity time?
+--       daysLeft = diffDays lastDayValid currentDay
+--   return $ daysLeft > 0
 
 main :: IO ()
 main = do
@@ -39,19 +45,3 @@ main = do
   putStrLn $ "download dir based on current time is " ++ dlDir
   let cDay = dirToDay dlDir
   putStrLn $ "converting it back to a Day, we get " ++ show cDay
-  let plus = addDays 90 cDay
-      minus = addDays (-90) cDay
-  putStrLn $ "so current downloads are valid until " ++ show plus
-  putStrLn $ "and today, downloads after " ++ show minus ++ " are valid"
-  let old = fromGregorian 2019 01 01
-      new = fromGregorian 2020 09 01
-      future = fromGregorian 2025 01 01
-  oldV <- isCachedDataValid old
-  putStrLn $ "is data from " ++ show old ++ " valid? " ++ show oldV
-  newV <- isCachedDataValid new
-  putStrLn $ "is data from " ++ show new ++ " valid? " ++ show newV
-  futureV <- isCachedDataValid future
-  putStrLn $ "is data from " ++ show future ++ " valid? " ++ show futureV
-
-  -- TODO looking good! next step is rewrite blastdblist to use it?
-  -- TODO and separate filter into "find" or something
