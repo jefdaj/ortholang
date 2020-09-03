a8d340cdf50f6723cba6d91c100dae1ad1e64664
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-09-03 11:03:25 -0700

    wip: got user-specific cache dirs working

--- time-test.hs
+++ time-test.hs
@@ -10,17 +10,21 @@ import Data.Time
 import Text.Printf
 import System.FilePath ((</>))
 import Data.List.Split (splitOn)
+import OrthoLang.Util (absolutize, globFiles)
+import Data.List (sort)
 
 nowDatedDir :: IO FilePath
 nowDatedDir = do
   (UTCTime date _) <- getCurrentTime
-  let (year, month, day) = toGregorian date
-      sYear  = printf "%04d" year
-      sMonth = printf "%02d" month
-      sDay   = printf "%02d" day
-      -- sSecs  = show (round secs :: Integer)
-      dir    = sYear </> sMonth </> sDay -- </> sSecs
-  return dir
+  return $ dayToDir date
+
+dayToDir :: Day -> FilePath
+dayToDir date = sYear </> sMonth </> sDay
+  where
+    (year, month, day) = toGregorian date
+    sYear  = printf "%04d" year
+    sMonth = printf "%02d" month
+    sDay   = printf "%02d" day
 
 -- TODO return a Maybe?
 dirToDay :: FilePath -> Day
@@ -31,12 +35,48 @@ dirToDay dir = fromGregorian nYear nMonth nDay
     nMonth = read month :: Int
     nDay   = read day   :: Int
 
--- isCachedDataValid :: Day -> IO Bool
--- isCachedDataValid dayFetched = do
---   (UTCTime currentDay _) <- getCurrentTime
---   let lastDayValid = addDays 90 dayFetched -- TODO configure the validity time?
---       daysLeft = diffDays lastDayValid currentDay
---   return $ daysLeft > 0
+parseDate :: String -> Maybe Day
+parseDate = parseTimeM True defaultTimeLocale "%Y-%m-%d"
+
+parseDateToday :: String -> IO Day
+parseDateToday input = do
+  today <- getCurrentDay
+  if input == "today" then return today
+  else case parseDate input of
+    Just day -> return day
+    Nothing -> do
+      putStrLn $ "Invalid date '" ++ input ++ "'. Defaulting to the current UTC date."
+      return today
+
+getCurrentDay :: IO Day
+getCurrentDay = do
+  (UTCTime today _) <- getCurrentTime
+  return today
+
+cacheDirDated :: FilePath -> String -> IO FilePath
+cacheDirDated cacheDir date = do
+  day <- parseDateToday date
+  cacheDir' <- absolutize cacheDir
+  return $ cacheDir' </> dayToDir day
+
+-- warning: assumes all files in the cache dir are yyyy/mm/dd formatted
+cacheDirLatest :: FilePath -> IO (Maybe FilePath)
+cacheDirLatest cacheDir = do
+  matches <- globFiles $ cacheDir </> "*" </> "*" </> "*" 
+  if null matches then return Nothing
+  else return $ Just $ head $ sort matches
+
+-- | Entry point for finding a cache dir from a user-specified date
+cacheDirUser :: FilePath -> String -> IO FilePath
+cacheDirUser cacheDir input = do
+  latest <- cacheDirLatest cacheDir
+  case input of
+    "latest" -> case latest of
+      Nothing -> do
+        putStrLn $ "No existing cache in '" ++ cacheDir ++ "'. Defaulting to the current UTC date."
+        cacheDirDated cacheDir "today"
+      Just d  -> return d
+    _ -> cacheDirDated cacheDir input
 
 main :: IO ()
 main = do
