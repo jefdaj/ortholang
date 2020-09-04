e886f94c237a9d89d3f8cd66e827a859bc88acc3
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-09-03 18:34:14 -0700

    todos

--- time-test.hs
+++ time-test.hs
@@ -3,6 +3,26 @@ module Main where
 -- TODO have cacheUser check the cfg and default to today if we're re-downloading everything
 -- TODO add a warning message when we're about to re-download
 
+-- TODO is "latest" the wrong word? maybe "cached" makes more sense or "latest-cached" or "previous" or "most-recent-cached" or "last"
+--        doesn't matter for now, because the implementation is the same
+
+-- TODO modules to integrate: Curl, BlastDB, BioMartR, Busco, *not* Load (maybe use curl for that)
+
+-- TODO Curl integration: curl "date" -> curl_path "dst path"
+--        could have an ortholang curl fn : str str -> untyped, and allow either untyped or str in load fns
+--        first str would be the date, second the url
+--        might be good to implement this first because blastdb + biomartr could be made to use it? check on that
+
+-- TODO BlastDB integration: blastdblist "date" -> rules for today's dblist.txt, load_list <dblist.txt path>?
+--        separate this into its own module? it's different from and built on top of blastdb
+--        the blastdblist and blastdbget fns should both have date strs because you might want to list newer than get
+
+-- TODO BioMartR integration: biomartr_cache "date" -> biomartr_path "dst path", biomartr_search "date"?
+--        seems like you have to download stuff for each search but there's also an overall list of txt files? see if really both
+
+-- TODO Busco integration: busco_list_lineages "date" -> busco_list_lineages_path "cache path"
+--        actually you could hold off on this one for now as they seem to be static
+
 import Data.Time
 import Text.Printf
 import System.FilePath ((</>))
