64c2b3110eb09c0e51cc00ff94cf2fdb32e62d93
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-09-12 15:51:02 +0000

    clean up Diamond.hs; rewrite done for now?

--- OrthoLang/Modules/Diamond.hs
+++ OrthoLang/Modules/Diamond.hs
@@ -7,6 +7,7 @@ module OrthoLang.Modules.Diamond
 
 -- TODO generate _rbh variants from regular + _rev ones?
 --      seems like a relatively straightforward edit using newExprExpansion now!
+--      could probably be done via something exported from BlastRBH
 
 import Development.Shake
 import OrthoLang.Types
@@ -23,17 +24,6 @@ import System.Process          (readProcess)
 import Data.Maybe (fromJust)
 import Data.Scientific        (Scientific)
 
--- type RulesFn     = RulesFn
--- type OldDiamondBlastDesc =
---   ( String              -- name
---   , [String] -> RulesFn -- rules, which will take cli args
---   , [String]            -- cli args
---   , Type                -- query type
---   , Type                -- subject type
---   , Type                -- result type
---   )
-
--- TODO anything else needed?
 type NewDiamondBlastDesc =
   ( String -- base name
   , Type   -- query type
@@ -41,15 +31,9 @@ type NewDiamondBlastDesc =
   , Type   -- return type (TODO remove?)
   )
 
--- variants :: [NewDiamondDesc]
--- variants =
---   [ ("blastp", faa, dmnd)
---   , ("blastx", fna, dmnd)
---   ]
-
--- TODO generate base functions
--- TODO generate _rev variants
--- TODO generate _each variants
+------------
+-- module --
+------------
 
 olModule :: Module
 olModule = Module
@@ -83,28 +67,18 @@ olModule = Module
       , diamondBlastpRev
       , diamondBlastxRev
 
-      -- _each variants
+      -- _each variants of the other 3
       , diamondBlastpEach
       , diamondBlastxEach
       , diamondBlastpDbEach
       , diamondBlastxDbEach
       , diamondBlastpDbRevEach
       , diamondBlastxDbRevEach
+
+      -- TODO _all variants
+      -- TODO _rbh variants
+
       ]
-
---       ++ map mkDiamondEach
---          -- _db_each
---           [ ("blastp_db", ["blastp"], faa, dmnd, bht)
---           , ("blastx_db", ["blastx"], fna, dmnd, bht)
---           -- _each (fa variants)
---           , ("blastp", ["blastp"], faa, faa , bht)
---           , ("blastx", ["blastx"], fna, faa , bht)
---           -- TODO db rev?
---           -- _rev_each (fa variants)
---           , ("blastp_rev", ["blastp"], faa, faa , bht)
---           , ("blastx_rev", ["blastx"], faa, fna , bht)
---           ]
-
   }
 
 -- TODO figure out how to prettyCat/show/whatever the encoded types, probably with a typeclass
@@ -118,7 +92,6 @@ dmnd = Type
       let d = unlines $ ("DIAMOND database " ++ path) : (drop 4 $ lines out)
       return d
   }
-
 
 ---------------------
 -- diamond_makedb* --
@@ -157,7 +130,6 @@ aDiamondmakedbAll (ExprPath out') fasPath' = do
   faPaths <- readPaths loc fasPath'
   aSimpleScriptPar "diamond_makedb_all.sh" (out:faPaths)
 
-
 -----------------
 -- sensitivity --
 -----------------
@@ -191,36 +163,9 @@ readSensitivity numPath = do
       flag = fromJust $ lookup n' sensitivityFlags
   return flag
 
-------------------------
--- old code to remove --
-------------------------
-
--- TODO can some of these be replaced by a numeric sensitivity arg?
--- oldVariants :: [OldDiamondBlastDesc]
--- oldVariants =
--- 
---   -- TODO rewrite with a new api function "newFlip23"
---   [ ("blastp_rev"   , rFlip23 . rDiamondFromFa          , ["blastp"], faa, faa , bht)
---   , ("blastx_rev"   , rFlip23 . rDiamondFromFa          , ["blastx"], faa, fna , bht)
---   , ("blastp_db_rev", rFlip23 . rSimple . aDiamondFromDb, ["blastp"], dmnd, faa, bht)
---   , ("blastx_db_rev", rFlip23 . rSimple . aDiamondFromDb, ["blastx"], dmnd, fna, bht)
--- 
---   -- TODO these seem to work, but do they make any sense to include?
---   -- , ("blastp_db_rev_each"               , rFlip23 . rMap 2 . aDiamondFromDb, ["blastp"                    ], dmnd, ListOf faa, ListOf bht)
---   -- , ("blastx_db_rev_each"               , rFlip23 . rMap 2 . aDiamondFromDb, ["blastx"                    ], dmnd, ListOf fna, ListOf bht)
---   ]
-
--- type NewDiamondBlastDesc =
-  -- ( String   -- name
-  -- , [String] -- cli args
-  -- , Type     -- query type
-  -- , Type     -- subject type
-  -- , Type     -- result type
-  -- )
-
---------------------
--- single from db --
---------------------
+---------------------------
+-- diamond_blast{p,x}_db --
+---------------------------
 
 diamondBlastpDb = mkDiamondFromDb ("blastp", faa, dmnd, bht)
 diamondBlastxDb = mkDiamondFromDb ("blastx", fna, dmnd, bht)
@@ -256,7 +201,7 @@ aDiamondFromDb bCmd (ExprPath o') s' e' q' db' = do
   sanitizeFileInPlace o'
 
 ------------------------
--- single fa variants --
+-- diamond_blast{p,x} --
 ------------------------
 
 diamondBlastp = mkDiamondFa ("blastp", faa, faa, bht)
@@ -276,9 +221,9 @@ mDiamondMakedb _ _ (Fun r ms ds n [i1, i2, i3, i4]) = Fun r ms ds (n ++ "_db") [
     fn = Fun dmnd ms (depsOf i4) "diamond_makedb" [i4]
 mDiamondMakedb _ _ e = error "modules.diamond.mDiamondMakedb" $ "bad argument: " ++ show e
 
--------------------
--- _rev variants --
--------------------
+-------------------------------
+-- diamond_blast{p,x}_db_rev --
+-------------------------------
 
 -- TODO do these make sense to include? maybe for the _rev_each variants?
 
@@ -293,21 +238,21 @@ mkDiamondRev old new (name, qType, sType, rType) = newExprExpansion
   (mFlip34 old new)
   [Nondeterministic]
 
--- TODO move to NewRules.hs?
+-- TODO move to NewRules.hs? if so, probably work on variable number of args?
 mFlip34 :: String -> String -> ExprExpansion
 mFlip34 old new _ _ (Fun r ms ds n [i1, i2, i3, i4]) = Fun r ms ds (replace old new n) [i1, i2, i4, i3]
 mFlip34 _ _ _ _ e = error "modules.diamond.mFlip34" $ "bad argument: " ++ show e
 
-------------------
--- _rev from fa --
-------------------
+----------------------------
+-- diamond_blast{p,x}_rev --
+----------------------------
 
 diamondBlastpRev = mkDiamondRev "_rev" "" ("blastp", faa, dmnd, bht)
 diamondBlastxRev = mkDiamondRev "_rev" "" ("blastp", faa, dmnd, bht)
 
---------------------
--- _each variants --
---------------------
+-------------------------
+-- diamond_blast*_each --
+-------------------------
 
 diamondBlastpEach      = mkDiamondEach "_each"        ""        ("blastp", faa, faa , bht)
 diamondBlastxEach      = mkDiamondEach "_each"        ""        ("blastx", fna, faa , bht)
@@ -339,69 +284,9 @@ mkDiamondEach old new (base, qType, sType, rType) =
 
 -- TODO write these
 
-
-  -- _each variants
-  -- map (\)
-
- -- TODO straighten out the _db naming stuff!
--- mkDiamondBlastFromFa :: String -> Type -> Type -> Type -> Function
--- mkDiamondBlastFromFa name qType faType dbType = newExprExpansion
---   (replace "_db" "" name)
---   [Exactly num, Exactly qType, Exactly faType]
---   (Exactly bht)
---   (mDiamondBlastFromFa name dbType)
---   [Nondeterministic]
-
--- TODO can this from BlastRBH be made into the flip fn?
--- mBlastFromFaRev :: ExprExpansion
--- mBlastFromFaRev _ _ (Fun r ms ds n [e, q, s]) = Fun r ms ds (replace "_rev" "" n) [e, s, q]
--- mBlastFromFaRev _ _ e = error "ortholang.modules.blastrbh.mBlastFromFaRev" $ "bad argument: " ++ show e
-
--- TODO straighten out the _db naming stuff!
--- mDiamondBlastFromFa :: String -> Type -> ExprExpansion
--- mDiamondBlastFromFa name dbType _ _ (Fun r ms ds _ [e,q,s]) = Fun r ms ds name1 [e,q,expr]
---   where
---     np = if dbType == fna then "nucl" else "prot"
---     name1 = replace "_db" "" name
---     name2 = "makeblastdb_" ++ np -- TODO would the _all version withSingleton arg be better?
---     expr = Fun (EncodedAs blastdb dbType) Nothing (depsOf s) name2 [s]
--- mDiamondBlastFromFa _ _ _ _ e = error "ortholang.modules.blast.mkDiamondBlastFromFa" $ "bad arg: " ++ show e
-
--- | This is the main entrypoint for generating an (old-style) OrthoLang function from the description
--- mkDiamondBlast :: OldDiamondBlastDesc -> Function
--- mkDiamondBlast (name, rFn, dCmd, qType, sType, rType) = let name' = "diamond_" ++ name in Function
---   { fOpChar = Nothing, fName = name'
---   , fInputs = [Exactly num, Exactly qType, Exactly sType]
---   , fOutput = Exactly rType
---   , fTags = [Nondeterministic] -- TODO double check: is it deterministic?
---   , fNewRules = NewNotImplemented, fOldRules = rFn dCmd
---   }
-
--- TODO rewrite as NewAction3 -> NewAction3 for here only for now
--- TODO make into a more general utility?
--- rFlip23 :: RulesFn -> RulesFn
--- rFlip23 rFn scr (Fun rtn seed deps ids args) = rFn scr (Fun rtn seed deps ids $ fn args)
---   where
---     fn (one:two:three:rest) = (one:three:two:rest)
---     fn as = error $ "bad argument to rFlip23: " ++ show as
--- rFlip23 _ _ e = error $ "bad argument to rFlip23: " ++ show e
-
--- aDiamondFromDb _ _ = error $ "bad argument to aDiamondFromDb"
-
--- inserts a "makedb" call and reuses the _db compiler from above
--- based on the version in Blast.hs but a little simpler
--- rDiamondFromFa :: [String] -> RulesFn
--- rDiamondFromFa dCmd st (Fun rtn seed deps _ [e, q, s])
---   = rules st (Fun rtn seed deps name1 [e, q, dbExpr])
---   where
---     rules  = rSimple $ aDiamondFromDb dCmd
---     name1  = "diamond_" ++ headOrDie "failed to parse dCmd in rDiamondFromFa" dCmd
---     dbExpr = Fun dmnd seed (depsOf s) "diamond_makedb" [s]
--- rDiamondFromFa _ _ _ = fail "bad argument to rDiamondFromFa"
-
--- TODO could this be rewritten to also work on other blast-like fns, for example in psiblast?
--- mDiamondFromFa :: ExprExpansion
--- mDiamondFromFa
+----------------------------
+-- old code for reference --
+----------------------------
 
 -- TODO separate into two things: an exprExpansion and a newMap
 -- same, but inserts a "makedb_each" call and maps over it
