60306ba186d1d413ea176c487cf6e4bddcb1a0c7
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-06-02 17:31:23 -0700

    justorthologs: todo and docs

--- OrthoLang/Modules/JustOrthologs.hs
+++ OrthoLang/Modules/JustOrthologs.hs
@@ -1,3 +1,11 @@
 module OrthoLang.Modules.JustOrthologs where
 
 olModule = undefined
+
+-- TODO jof type "justorthologs format" (sorted fasta coding sequences with the asterisks)
+-- TODO justorthologs_format : gff faa -> jff to extract the proper cds format from just one gff3 + reference genome pair
+-- TODO set -t to match nproc
+-- TODO search functions: justorthologs_related, justorthologs_distant, justorthologs (combined)
+-- TODO what's the correlation value, and should the search fns expose it?
+-- TODO orthogroups function to extract them from the output (or is it just pairs here?)
+-- TODO express the optional compression of the input fasta files
