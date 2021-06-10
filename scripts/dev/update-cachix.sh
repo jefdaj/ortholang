be97ce969b8ac3f40e4f74a937b0324ecd6f97f2
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-06-09 19:20:23 -0700

    update-cachix.sh: rm .stack-work first

--- scripts/dev/update-cachix.sh
+++ scripts/dev/update-cachix.sh
@@ -4,4 +4,7 @@
 # see https://github.com/cachix/cachix/issues/52
 # And of course, it will only work if you have my API key.
 
+# this prevents accidentally including it in the upload
+rm -rf .stack-work
+
 nix-store -qR --include-outputs $(nix-instantiate release.nix) | cachix push jefdaj
