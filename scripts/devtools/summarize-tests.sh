ef419612a235b9bb48605cf8a6788302c8747f89
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-06-17 10:28:30 -0700

    summarize-tests.sh: show more test files

--- scripts/devtools/summarize-tests.sh
+++ scripts/devtools/summarize-tests.sh
@@ -2,12 +2,18 @@
 
 LOG=test.log
 
-echo -e "tests passed:\t$(grep OK $LOG | wc -l)"
+echo -e "tests passed:           \t$(grep OK $LOG | wc -l)"
 echo
-echo -e "tests timed out:\t$(grep TIMEOUT $LOG | wc -l)"
-echo -e "important tests failed:\t$(grep FAIL $LOG | grep -v help | wc -l)"
-echo -e "trivial tests failed:\t$(grep FAIL $LOG | grep    help | wc -l)"
+echo -e "tests timed out:        \t$(grep TIMEOUT $LOG | wc -l)"
+echo -e "important tests failed: \t$(grep FAIL $LOG | grep -v help | wc -l)"
+echo -e "trivial tests failed:   \t$(grep FAIL $LOG | grep    help | wc -l)"
 echo
-echo -e "tmpfiles changed:\t$(git status | grep tests | grep tmpfiles | wc -l)"
-echo -e "stdouts changed:\t$(git status | grep tests | grep stdout | wc -l)"
-echo -e "expands changed:\t$(git status | grep tests | grep expand | wc -l)"
+echo -e "check changed:          \t$(git status | grep tests | grep check | wc -l)"
+echo -e "expand changed:         \t$(git status | grep tests | grep expand | wc -l)"
+echo -e "help changed:           \t$(git status | grep tests | grep help | wc -l)"
+echo -e "parse changed:          \t$(git status | grep tests | grep parse | wc -l)"
+echo -e "repl changed:           \t$(git status | grep tests | grep repl | wc -l)"
+echo -e "scripts changed:        \t$(git status | grep tests | grep scripts | wc -l)"
+echo -e "stdout changed:         \t$(git status | grep tests | grep stdout | wc -l)"
+echo -e "tmpfiles changed:       \t$(git status | grep tests | grep tmpfiles | wc -l)"
+echo -e "versions changed:       \t$(git status | grep tests | grep versions | wc -l)"
