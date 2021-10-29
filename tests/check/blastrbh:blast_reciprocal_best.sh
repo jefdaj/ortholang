61c1cb75f3ee51847ed1de9a71e2a94685965d50
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-10-29 10:24:17 -0700

    update tests

--- tests/check/blastrbh:blast_reciprocal_best.sh
+++
@@ -1,5 +0,0 @@
-# TODO is it actually always supposed to be 203?
-# The number of hits varies pretty wildly, but there should at least be a few!
-output="$(cat "${1}/output.txt" | cut -d'.' -f1)"
-minhits=3
-[[ $output -ge $minhits ]] || (echo -n "less than $minhits hits: $output" && exit 1)
